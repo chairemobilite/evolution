b5d064f911ebc8443d5e8de3f48bd5ea1bf3c0bf
Geneviève Bastien <gbastien@versatic.net>
2023-01-11 14:05:25 -0500

    Make `compile`, `format`, `lint` and `test` work
    Add the locales files from transition legacy in the `locales` folder
    and test configuration and other necessary files in the `tests` folder.
    
    Add eslint, prettier and base tsconfig files from legacy.
    
    Update paths for this new repo.

---
+++ tests/config_test.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+    projectDirectory: `${__dirname}/runtime/`,
+    projectShortname: 'test',
+  
+    allowRegistration: true,
+  
+    mapDefaultCenter: {
+      lat: 45.503205,
+      lon: -73.569417
+    },
+  
+    languages: ['fr', 'en'],
+    
+    locales: {
+      fr: 'fr-CA',
+      en: 'en-CA'
+    },
+  
+    languageNames: {
+      fr: "Français",
+      en: "English"
+    },
+  
+    title: {
+      fr: "Test",
+      en: "Test"
+    },
+    
+    defaultLocale: "fr",
+    timezone: "America/Montreal",
+};
+  
