8e9a24ba4f67cf76cf73935eedf201130d656c78
Geneviève Bastien <gbastien@versatic.net>
2023-05-24 19:48:48 -0400

    question: Extract the help popup link to a widget
    This widget manages the modal window opening and thus reduces the
    responsibilities of the Question widget.

---
+++ packages/evolution-frontend/src/components/survey/widgets/HelpPopupLink.tsx
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons/faQuestionCircle';
+
+import SimpleModal from '../../modal/SimpleModal';
+
+interface HelpPopupLinkProps {
+    containsHtml: boolean;
+    /** Text shown on the link and title of the modal */
+    title: string;
+    /** Text displayed in the help modal, function because it will only be retrieved on demand */
+    content: () => string;
+}
+
+export const HelpPopupLink = (props: HelpPopupLinkProps) => {
+    const [modalIsOpened, setModalIsOpened] = React.useState(false);
+    return (
+        <div>
+            {modalIsOpened && (
+                <SimpleModal
+                    isOpen={true}
+                    closeModal={() => setModalIsOpened(false)}
+                    text={props.content()}
+                    title={props.title}
+                    containsHtml={props.containsHtml}
+                />
+            )}
+            <button
+                type="button"
+                className="button helper-popup blue small"
+                onClick={() => setModalIsOpened(true)}
+                tabIndex={-1}
+            >
+                <FontAwesomeIcon icon={faQuestionCircle} className="faIconLeft" />
+                {props.title}
+            </button>
+        </div>
+    );
+};
+
+export default HelpPopupLink;
