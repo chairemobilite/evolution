14a4b2ffaf74e488cfd0173eaaa3189da08f1018
Geneviève Bastien <gbastien@versatic.net>
2023-08-14 18:02:40 -0400

    interviewer: Add interviewer monitoring route and component
    * Add a frontend monitoring component, which allows to select a start
      and end date, then update the interviewer cache and downloads the file
    
    * Add a db query to fetch all interviews touched by interviewers, within
      a certain period and as a paginated list
    
    * Application that use this interviewer monitoring module need to call a
      function in `interviewer.routes.ts`, which will put in place the
    necesary routes for interviewer data. This function receives as
    parameter an object with the following functions:
    
    {
      `data: (responses) => object` which receives the responses of an
    individual interview and should return an object with specific fields to
    cache for this interview.
    
      `aggregate: (dataForInterview: any[]) => object` which receives all
    the parsed interview data for a single interviewer and can aggregate
    statistical data from those. For example, if a field for the interview
    is a counter (number of x), the aggregate function could return a field
    that would be a sum, or an average of those counters.
    }

---
+++ packages/evolution-interviewer/src/client/components/monitoring/RefreshInterviewerCache.tsx
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2023, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import moment from 'moment';
+import React from 'react';
+import { withTranslation, WithTranslation } from 'react-i18next';
+import _max from 'lodash.max';
+import Button from 'evolution-legacy/lib/components/survey/Button';
+import LoadingPage from 'chaire-lib-frontend/lib/components/pages/LoadingPage';
+import DatePicker from 'chaire-lib-frontend/lib/components/input/Calendar';
+
+export const RefreshInterviewerCache = (props: WithTranslation) => {
+    const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState<undefined | string | null>(undefined);
+    const [start, setStart] = React.useState(moment().subtract(1, 'weeks'));
+    const [end, setEnd] = React.useState(moment());
+
+    const onRefreshMonitoringData = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch('/interviewer/monitoring/update_interviewer_cache', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ start: start.unix(), end: end.unix() })
+            });
+            if (response.status === 200) {
+                await response.json();
+                setError(undefined);
+            } else if (response.status === 401) {
+                setError(props.t(['survey:admin:unauthorized', 'admin:unauthorized']));
+            } else {
+                setError(props.t(['survey:admin:interviewer:error', 'admin:interviewer:error']));
+            }
+        } catch (error) {
+            setError(props.t(['survey:admin:interviewer:error', 'admin:interviewer:error']));
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="admin-widget-container">
+            {loading && <LoadingPage />}
+            <DatePicker
+                id="admin_interviewer_date_from"
+                onChange={(s, e) => {
+                    setStart(moment(s));
+                    setEnd(moment(e));
+                }}
+                startDate={start.format('YYYY-MM-DD')}
+                endDate={end.format('YYYY-MM-DD')}
+                dateFormat="YYYY-MM-DD"
+                language={props.i18n.language}
+            />
+            <Button
+                widgetConfig={{
+                    label: props.t([
+                        'survey:admin:interviewer:refreshInterviewerCache',
+                        'admin:interviewer:refreshInterviewerCache'
+                    ]),
+                    color: 'blue',
+                    action: () => {
+                        onRefreshMonitoringData();
+                    },
+                    hideWhenRefreshing: true
+                }}
+                widgetStatus={{
+                    isVisible: true
+                }}
+                loadingState={loading ? 1 : 0}
+            />
+            {error && <p className="survey-question__error_message">{error}</p>}
+            <a href="/interviewer/monitoring/get_interviewer_data">Interviewer data</a>
+        </div>
+    );
+};
+
+export default withTranslation()(RefreshInterviewerCache);
