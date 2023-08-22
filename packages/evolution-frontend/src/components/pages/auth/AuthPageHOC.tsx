3c0bacf8f84d8259222190148e1d4c2c7faa1977
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2023-08-22 12:51:04 -0400

    allow the use of enter key or space bar when a button is focused
    This is for better form accessibility. Note that the enter key is still disabled if the button is not focused with the tab key, except in the login page.

--- packages/evolution-frontend/src/components/pages/auth/AuthPageHOC.tsx
+++ packages/evolution-frontend/src/components/pages/auth/AuthPageHOC.tsx
@@ -47,34 +47,26 @@ const authPageHOC = <P extends AuthPageProps & WithTranslation>(WrappedComponent
             if (props.isAuthenticated) {
                 props.history.push(appConfiguration.homePage);
             }
-            document.addEventListener('keydown', onKeyPress.bind(this));
-            return () => {
-                document.removeEventListener('keydown', onKeyPress.bind(this));
-            };
         }, []);
 
         let surveyContainerStyle = {};
         if (config.logoPaths) {
             surveyContainerStyle = {
-                backgroundImage: `url(${config.logoPaths[props.i18n.language]}), url(/dist/images/ornaments/ornament_flower_points_pale.svg)`,
+                backgroundImage: `url(${
+                    config.logoPaths[props.i18n.language]
+                }), url(/dist/images/ornaments/ornament_flower_points_pale.svg)`,
                 backgroundSize: '15rem, 6rem',
                 backgroundPosition: 'left 1rem bottom 1rem, right 1rem top 100.5%',
                 backgroundRepeat: 'no-repeat, no-repeat'
             };
         }
 
-        const onKeyPress = (e) => {
-            if (e.which === 13 && e.target.tagName.toLowerCase() !== 'textarea' /* Enter */) {
-                e.preventDefault();
-            }
-        };
-
-        const selectAnonymousLogin = (e: React.MouseEvent) => {
+        const selectAnonymousLogin = (e: React.MouseEvent | React.KeyboardEvent) => {
             e.preventDefault();
             setAnonymousRequested(true);
         };
 
-        const selectAuthMethod = (authMethod: string) => (e: React.MouseEvent) => {
+        const selectAuthMethod = (authMethod: string) => (e: React.MouseEvent | React.KeyboardEvent) => {
             e.preventDefault();
             setCurrentAuthMethod(authMethod);
         };
@@ -85,11 +77,23 @@ const authPageHOC = <P extends AuthPageProps & WithTranslation>(WrappedComponent
             <div className="survey" style={surveyContainerStyle}>
                 <div style={{ width: '100%', margin: '0 auto', maxWidth: '90em' }}>
                     <div className="apptr__separator"></div>
-                    <p className='_center _large' style={{ marginBottom: '1rem', paddingLeft: '2rem', paddingRight: '2rem' }}>{props.t(['survey:auth:AuthMainIntroduction', 'auth:AuthMainIntroduction'])}</p>
-                    <div style={{ width: '100%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'stretch', alignContent: 'flex-start' }}>
-
-
-                        <div className='apptr__auth-box'>
+                    <p
+                        className="_center _large"
+                        style={{ marginBottom: '1rem', paddingLeft: '2rem', paddingRight: '2rem' }}
+                    >
+                        {props.t(['survey:auth:AuthMainIntroduction', 'auth:AuthMainIntroduction'])}
+                    </p>
+                    <div
+                        style={{
+                            width: '100%',
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            justifyContent: 'center',
+                            alignItems: 'stretch',
+                            alignContent: 'flex-start'
+                        }}
+                    >
+                        <div className="apptr__auth-box">
                             <WrappedComponent
                                 {...(restProps as P)}
                                 authMethod={currentAuthMethod}
@@ -98,85 +102,126 @@ const authPageHOC = <P extends AuthPageProps & WithTranslation>(WrappedComponent
                             <div className="apptr__separator"></div>
                             {currentAuthMethod !== 'passwordless' && authMethods.includes('passwordless') && (
                                 <div>
-                                    <a className="register-link _oblique" href="#" onClick={selectAuthMethod('passwordless')}>
-                                        {props.t(['survey:auth:UsePasswordLessAuthMethod', 'auth:UsePasswordLessAuthMethod'])}
+                                    <a
+                                        className="register-link _oblique"
+                                        href="#"
+                                        onClick={selectAuthMethod('passwordless')}
+                                    >
+                                        {props.t([
+                                            'survey:auth:UsePasswordLessAuthMethod',
+                                            'auth:UsePasswordLessAuthMethod'
+                                        ])}
                                     </a>
                                 </div>
                             )}
                             {currentAuthMethod !== 'localLogin' && authMethods.includes('localLogin') && (
                                 <div>
-                                    <a className="register-link _oblique" href="#" onClick={selectAuthMethod('localLogin')}>
-                                        {props.t(['survey:auth:UseLocalLoginAuthMethod', 'auth:UseLocalLoginAuthMethod'])}
+                                    <a
+                                        className="register-link _oblique"
+                                        href="#"
+                                        onClick={selectAuthMethod('localLogin')}
+                                    >
+                                        {props.t([
+                                            'survey:auth:UseLocalLoginAuthMethod',
+                                            'auth:UseLocalLoginAuthMethod'
+                                        ])}
                                     </a>
                                 </div>
                             )}
                         </div>
 
-
-                        {(config.connectWithGoogle || config.auth?.google || config.connectWithFacebook || config.auth?.facebook) && <div className='apptr__auth-box'>
-                            <p className="_label apptr__form__label-standalone">{props.t(['survey:auth:OAuthTitle', 'auth:OAuthTitle'])}</p>
-                            {/*<div className={'apptr__form-label-container apptr_form-oauth-introduction'}>
+                        {(config.connectWithGoogle ||
+                            config.auth?.google ||
+                            config.connectWithFacebook ||
+                            config.auth?.facebook) && (
+                            <div className="apptr__auth-box">
+                                <p className="_label apptr__form__label-standalone">
+                                    {props.t(['survey:auth:OAuthTitle', 'auth:OAuthTitle'])}
+                                </p>
+                                {/*<div className={'apptr__form-label-container apptr_form-oauth-introduction'}>
                                 <div className="apptr__form__label-standalone no-question">
                                     <p className="_green _strong">{props.t(['survey:auth:OAuthIntroduction', 'auth:OAuthIntroduction'])}</p>
                                 </div>
                             </div>*/}
-                            {(config.connectWithGoogle || config.auth?.google) && (
-                                <div className="google-oauth-button-container apptr_form-oauth-google-login">
-                                    <a className="google-oauth-button" href="/googlelogin">
-                                        {props.t('auth:signInWithGoogle')}
-                                    </a>
-                                </div>
-                            )}
-                            {(config.connectWithFacebook || config.auth?.facebook) && (
-                                <div className="facebook-oauth-button-container apptr_form-oauth-facebook-login">
-                                    <a className="facebook-oauth-button" href="/facebooklogin">
-                                        {props.t('auth:signInWithFacebook')}
-                                    </a>
-                                </div>
-                            )}
-                        </div>}
-
-
-                        {config.auth.anonymous === true && <div className='apptr__auth-box'>
-                            {config.auth.anonymous === true &&
-                                (anonymousRequested === true ? (
-                                    <AnonymousLogin {...props} />
-                                ) : (
-                                    <div className={'apptr__form-label-container apptr_form-anonymous'}>
-                                        <p className="_label apptr__form__label-standalone">{props.t(['survey:auth:AuthAnonymousTitle', 'auth:AuthAnonymousTitle'])}</p>
-                                        <div className="apptr__form__label-standalone no-question apptr_form-anonymous-explanation">
-                                            <p >
-                                                <span className='_red'>{props.t([
-                                                    'survey:auth:Warning',
-                                                    'auth:Warning'
-                                                ])}</span><span className='_pale'>: {props.t([
-                                                    'survey:auth:AnonymousLoginExplanation',
-                                                    'auth:AnonymousLoginExplanation'
-                                                ])}</span>
-                                            </p>
-                                        </div>
-                                        <div className="apptr__form__label-standalone no-question apptr_form-anonymous-login">
-                                            <button
-                                                type="button"
-                                                className={'survey-section__button button green'}
-                                                onClick={selectAnonymousLogin}
-                                            >
-                                                {props.t(['survey:auth:UseAnonymousLogin', 'auth:UseAnonymousLogin'])}
-                                            </button>
-                                        </div>
+                                {(config.connectWithGoogle || config.auth?.google) && (
+                                    <div className="google-oauth-button-container apptr_form-oauth-google-login">
+                                        <a className="google-oauth-button" href="/googlelogin">
+                                            {props.t('auth:signInWithGoogle')}
+                                        </a>
                                     </div>
-                                ))}
-                        </div>}
+                                )}
+                                {(config.connectWithFacebook || config.auth?.facebook) && (
+                                    <div className="facebook-oauth-button-container apptr_form-oauth-facebook-login">
+                                        <a className="facebook-oauth-button" href="/facebooklogin">
+                                            {props.t('auth:signInWithFacebook')}
+                                        </a>
+                                    </div>
+                                )}
+                            </div>
+                        )}
 
+                        {config.auth.anonymous === true && (
+                            <div className="apptr__auth-box">
+                                {config.auth.anonymous === true &&
+                                    (anonymousRequested === true ? (
+                                        <AnonymousLogin {...props} />
+                                    ) : (
+                                        <div className={'apptr__form-label-container apptr_form-anonymous'}>
+                                            <p className="_label apptr__form__label-standalone">
+                                                {props.t(['survey:auth:AuthAnonymousTitle', 'auth:AuthAnonymousTitle'])}
+                                            </p>
+                                            <div className="apptr__form__label-standalone no-question apptr_form-anonymous-explanation">
+                                                <p>
+                                                    <span className="_red">
+                                                        {props.t(['survey:auth:Warning', 'auth:Warning'])}
+                                                    </span>
+                                                    <span className="_pale">
+                                                        :{' '}
+                                                        {props.t([
+                                                            'survey:auth:AnonymousLoginExplanation',
+                                                            'auth:AnonymousLoginExplanation'
+                                                        ])}
+                                                    </span>
+                                                </p>
+                                            </div>
+                                            <div className="apptr__form__label-standalone no-question apptr_form-anonymous-login">
+                                                <button
+                                                    type="button"
+                                                    className={'survey-section__button button green'}
+                                                    onClick={selectAnonymousLogin}
+                                                    onKeyUp={(e) => {
+                                                        if (
+                                                            e.key === 'enter' ||
+                                                            e.key === 'space' ||
+                                                            e.which === 13 ||
+                                                            e.which === 32
+                                                        ) {
+                                                            selectAnonymousLogin(e);
+                                                        } else {
+                                                            return;
+                                                        }
+                                                    }}
+                                                >
+                                                    {props.t([
+                                                        'survey:auth:UseAnonymousLogin',
+                                                        'auth:UseAnonymousLogin'
+                                                    ])}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    ))}
+                            </div>
+                        )}
                     </div>
-                    <div className={'apptr__form-label-container apptr_form-why-login-info'} style={{ paddingLeft: '2rem', paddingRight: '2rem' }}>
+                    <div
+                        className={'apptr__form-label-container apptr_form-why-login-info'}
+                        style={{ paddingLeft: '2rem', paddingRight: '2rem' }}
+                    >
                         <div className="apptr__form__label-standalone no-question _oblique">
                             <p>{props.t(['survey:auth:WhyLoginInfo', 'auth:WhyLoginInfo'])}</p>
                         </div>
                     </div>
-
                 </div>
-
             </div>
         );
     };
--- packages/evolution-legacy/src/components/survey/Survey.js
+++ packages/evolution-legacy/src/components/survey/Survey.js
@@ -43,7 +43,6 @@ export class Survey extends React.Component {
     this.openConfirmModal  = this.openConfirmModal.bind(this);
     this.closeConfirmModal = this.closeConfirmModal.bind(this);
     this.onChangeSection   = this.onChangeSection.bind(this);
-    this.onKeyPress        = this.onKeyPress.bind(this);
   }
 
   openConfirmModal(confirmModalShortname, e) {
@@ -72,13 +71,6 @@ export class Survey extends React.Component {
     Preferences.load().then(() => {
         this.setState({ preferencesLoaded: true })
     });
-  }
-
-  onKeyPress(e) {
-    if (e.which === 13 && e.target.tagName.toLowerCase() !== 'textarea' /* Enter */)
-    {
-      e.preventDefault();
-    }
   }
 
   onChangeSection(parentSection, activeSection, allWidgetsValid, e) {
@@ -181,7 +173,7 @@ export class Survey extends React.Component {
                   containsHtml       = {false}
                 />
               </div>)}
-              <form onKeyPress={this.onKeyPress} className="apptr__form" id="survey_form" style={customStyle} autoComplete="off">
+              <form onSubmit={(e) => { if (e.key === 'enter' || e.which === 13 ) { e.preventDefault(); }}} className="apptr__form" id="survey_form" style={customStyle} autocomplete="off">
                 <SectionComponent
                   key                         = {sectionShortname}
                   loadingState                = {this.props.loadingState}
