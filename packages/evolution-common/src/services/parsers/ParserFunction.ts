6c1cef7e689bc1364f97a9e1a5c6be7f59ed85d6
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2023-09-29 12:02:34 -0400

    add parser function and path config
    for better access to configuration of each path, which will be useful in the future when we will merge all widget/path configs for both the questionnaire and the admin with validations

---
+++ packages/evolution-common/src/services/parsers/ParserFunction.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+
+import { UserInterviewAttributes, InterviewResponses, Household, Person } from '../interviews/interview';
+
+/**
+ * Parser function to change/manipulate/enhance values for export.
+ * Associated with a specific path.
+ * TODO: update this parser made for legacy purpose, one step at a time,
+ * using new classes for person, household, etc. and after validations
+ * cleaning.
+ */
+export type ParserFunction<CustomSurvey, CustomHousehold, CustomHome, CustomPerson> = (
+    interview: UserInterviewAttributes<CustomSurvey, CustomHousehold, CustomHome, CustomPerson>,
+    responses: InterviewResponses<CustomSurvey, CustomHousehold, CustomHome, CustomPerson> & { [key: string]: any },
+    household: Household<CustomHousehold, CustomPerson>,
+    home: {
+        region?: string;
+        country?: string;
+    } & CustomHome,
+    personsById: { [key: string]: Person<CustomPerson> }, // kept for backward compatibility, will be replaced by object types
+    personsArray: Person<CustomPerson>[],// kept for backward compatibility, one of the two will be removed
+    person: Person<CustomPerson>
+) => any; // returns the new value after parsing
---
+++ packages/evolution-common/src/services/widgets/PathConfig.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+
+import { ValidationFunction } from './WidgetConfig';
+import { ParserFunction } from '../parsers/ParserFunction';
+
+/**
+ * This type describes the paths in the responses field,
+ * with data for validation and export.
+ * Path config include custom admin validations,
+ * flag to include the path in export files or not,
+ * and parsers for custom changes to data before export
+ * Keys are paths with uuids replaced by '@' and array indexes
+ * replaced by '#'
+ */
+export type PathConfig<CustomSurvey, CustomHousehold, CustomHome, CustomPerson> = {
+    // is the path metadata (included only in export files of type "with metadata")
+    isMetadata?: boolean; // default to false except if attribute starts with "_"
+    // whether to include the attribute when exporting:
+    // (metadata will be excluded even in metadata export files if set to true)
+    includeInExport?: boolean; // default to true
+    // whether to validate the attribute:
+    validate?: boolean; // default to true
+    // custom admin validations:
+    validations?: ValidationFunction<CustomSurvey, CustomHousehold, CustomHome, CustomPerson>;
+    // rename attribute for export, default to undefined, which means no rename:
+    rename?: string;
+    // whether to auto add validations from the widget:
+    includeWidgetValidations?: boolean; // default to true, ignored if validate is false
+    // custom parser to change/manipulate/enhance value for export:
+    parser?: ParserFunction<CustomSurvey, CustomHousehold, CustomHome, CustomPerson>;
+    // export generated values from this value, uses the same function as the parser:
+    // example: from _completedAt timestamp, generate completedAtDate and completedAtTime
+    generatedValues?: {
+        [key: string]: PathConfig<CustomSurvey, CustomHousehold, CustomHome, CustomPerson>
+    };
+};
