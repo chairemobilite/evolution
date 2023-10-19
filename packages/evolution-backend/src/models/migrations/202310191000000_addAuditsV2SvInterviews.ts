f2ac77f2def8058c9f666bdc9240630c3a4faa63
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2023-10-19 14:45:27 -0400

    add audit_v2 column to sv_interviews
    now it could be compatible with the new format, and the old format will still work

---
+++ packages/evolution-backend/src/models/migrations/202310191000000_addAuditsV2SvInterviews.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2023, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import { Knex } from 'knex';
+
+const interviewsTbl = 'sv_interviews';
+const columnName = 'audits_v2';
+
+export async function up(knex: Knex): Promise<unknown> {
+    return knex.schema.alterTable(interviewsTbl, (table: Knex.TableBuilder) => {
+        table.boolean(columnName).defaultTo(false).index();
+    });
+}
+
+export async function down(knex: Knex): Promise<unknown> {
+    return knex.schema.alterTable(interviewsTbl, (table: Knex.TableBuilder) => {
+        table.dropColumn(columnName);
+    });
+}
