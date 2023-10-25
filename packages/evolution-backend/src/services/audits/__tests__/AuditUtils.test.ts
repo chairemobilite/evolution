5165001dba0d4126783b6c26a7987ea2d711727a
Geneviève Bastien <gbastien@versatic.net>
2023-10-25 12:45:05 -0400

    audits: Move the audits to/from array functions to common
    These functions can be used by the frontend to convert the content
    received from the server to objects to display in the validation on
    pager.

--- packages/evolution-backend/src/services/audits/__tests__/AuditUtils.test.ts
+++ packages/evolution-backend/src/services/audits/__tests__/AuditUtils.test.ts
@@ -5,14 +5,12 @@
  * License text available at https://opensource.org/licenses/MIT
  */
 import { v4 as uuidV4 } from 'uuid';
-import _omit from 'lodash/omit';
 
-import { mergeWithExisting, convertParamsErrorsToAudits, auditArrayToAudits, auditsToAuditArray } from '../AuditUtils';
+import { mergeWithExisting, convertParamsErrorsToAudits } from '../AuditUtils';
 import slugify from 'slugify';
 import { Audits } from 'evolution-common/lib/services/audits/types';
 
 const arbitraryUuid = uuidV4();
-const arbitraryUuid2 = uuidV4();
 
 describe('convertParamsErrorsToAudits', () => {
     it('should convert error messages to audits', async () => {
@@ -91,102 +89,5 @@ describe('mergeWithExisting', () => {
         expect(mergedAudits['outdated-error']).toBeUndefined();
         expect(mergedAudits['version-changed-error'].version).toBe(2);
         expect(mergedAudits['version-changed-error'].ignore).toBe(false);
-    });
-});
-
-describe('auditArrayToAudits', () => {
-    it('should convert an audit array to object audits', async () => {
-
-        // Create audits for 3 objects, 2 audits for interview, 1 for each other object
-        const audits = [{
-            version: 1,
-            errorCode: 'test-error',
-            message: 'Test error message',
-            isWarning: false,
-            ignore: true,
-            objectType: 'interview',
-            objectUuid: arbitraryUuid
-        }, {
-            version: 1,
-            errorCode: 'test-error-2',
-            message: 'Test error message',
-            isWarning: false,
-            ignore: true,
-            objectType: 'interview',
-            objectUuid: arbitraryUuid
-        }, {
-            version: 1,
-            errorCode: 'test-error',
-            message: 'Test error message',
-            isWarning: false,
-            ignore: true,
-            objectType: 'person',
-            objectUuid: arbitraryUuid
-        }, {
-            version: 1,
-            errorCode: 'test-error',
-            message: 'Test error message',
-            isWarning: false,
-            ignore: true,
-            objectType: 'person',
-            objectUuid: arbitraryUuid2
-        }]
-
-        const perObjectAudits = auditArrayToAudits(audits);
-
-        expect(Object.keys(perObjectAudits).length).toEqual(3);
-        expect(perObjectAudits[`interview.${arbitraryUuid}`]).toEqual({
-            'test-error': _omit(audits[0], ['objectUuid', 'objectType']), 
-            'test-error-2': _omit(audits[1], ['objectUuid', 'objectType'])
-        });
-        expect(perObjectAudits[`person.${arbitraryUuid}`]).toEqual({
-            'test-error': _omit(audits[2], ['objectUuid', 'objectType'])
-        });
-        expect(perObjectAudits[`person.${arbitraryUuid}`]).toEqual({
-            'test-error': _omit(audits[3], ['objectUuid', 'objectType'])
-        })
-    });
-});
-
-describe('auditsToAuditArray', () => {
-    it('should convert an audits object to audit array', async () => {
-
-        const auditObject = {
-            'test-error': {
-                version: 1,
-                errorCode: 'test-error',
-                message: 'Test error message',
-                isWarning: false,
-                ignore: true
-            }, 'test-error-2': {
-                version: 1,
-                errorCode: 'test-error-2',
-                message: 'Test error message',
-                isWarning: false,
-                ignore: true
-            }
-        }
-        const expectedAudits = [{
-            version: 1,
-            errorCode: 'test-error',
-            message: 'Test error message',
-            isWarning: false,
-            ignore: true,
-            objectType: 'interview',
-            objectUuid: arbitraryUuid
-        }, {
-            version: 1,
-            errorCode: 'test-error-2',
-            message: 'Test error message',
-            isWarning: false,
-            ignore: true,
-            objectType: 'interview',
-            objectUuid: arbitraryUuid
-        }];
-
-        const auditsArr = auditsToAuditArray(auditObject, { objectType: 'interview', objectUuid: arbitraryUuid});
-
-        expect(auditsArr.length).toEqual(2);
-        expect(auditsArr).toEqual(expectedAudits);
     });
 });
