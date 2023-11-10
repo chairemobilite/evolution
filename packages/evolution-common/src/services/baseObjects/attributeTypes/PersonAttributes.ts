d87bf64fb2e6b1f476c0cec5bd5c3c9c1c0d2bae
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2023-11-09 19:39:36 -0500

    add missing choices to BasePerson.occupation
    also update person attributes choices to array + type for better usage + add older age groups

--- packages/evolution-common/src/services/baseObjects/attributeTypes/PersonAttributes.ts
+++ packages/evolution-common/src/services/baseObjects/attributeTypes/PersonAttributes.ts
@@ -8,73 +8,93 @@
 // Should be validated for max 120 years old:
 export type Age = number;
 
-export type AgeGroup =
-    | '0-4'
-    | '5-9'
-    | '10-14'
-    | '15-19'
-    | '20-24'
-    | '25-29'
-    | '30-34'
-    | '35-39'
-    | '40-44'
-    | '45-49'
-    | '50-54'
-    | '55-59'
-    | '60-64'
-    | '65-69'
-    | '70-74'
-    | '75-79'
-    | '80-84'
-    | '85-89'
-    | '90-94'
-    | '95-99'
-    | '100-104'
-    | '105-109'
-    | '110-114'
-    | '115-119';
+export const ageGroupValues = [
+    '0-4',
+    '5-9',
+    '10-14',
+    '15-19',
+    '20-24',
+    '25-29',
+    '30-34',
+    '35-39',
+    '40-44',
+    '45-49',
+    '50-54',
+    '55-59',
+    '60-64',
+    '65-69',
+    '70-74',
+    '75-79',
+    '80-84',
+    '85-89',
+    '90-94',
+    '95-99',
+    '100-104',
+    '105-109',
+    '110-114',
+    '115-119',
+    '120-124',
+    '125-129',
+];
+
+export type AgeGroup = typeof ageGroupValues[number];
 // Most of the time at least age OR age group must be defined
 
-export type Gender = 'female' | 'male' | 'custom';
+export const genderValues = [
+    'female',
+    'male',
+    'custom',
+];
+export type Gender = typeof genderValues[number];
 
-export type DrivingLicenseOwnership = 'yes' | 'no' | 'dontKnow' | 'nonApplicable';
 
-export type TransitPassOwnership = 'yes' | 'no' | 'dontKnow' | 'nonApplicable';
+export const yesNoDontKnowNonApplicableValues = ['yes', 'no', 'dontKnow', 'nonApplicable'];
+export type YesNoDontKnowNonApplicable = typeof yesNoDontKnowNonApplicableValues[number];
 
-export type CarsharingMember = 'yes' | 'no' | 'dontKnow' | 'nonApplicable';
+
+export type DrivingLicenseOwnership = YesNoDontKnowNonApplicable;
+
+export type TransitPassOwnership = YesNoDontKnowNonApplicable;
+
+export type CarsharingMember = YesNoDontKnowNonApplicable;
 
 // Be careful how the question is asked. Used carsharing service in the previous 30 days, previous 6 months?
 // This should denote whether a person can use carsharing service/knows how to use it:
-export type CarsharingUser = 'yes' | 'no' | 'dontKnow' | 'nonApplicable';
+export type CarsharingUser = YesNoDontKnowNonApplicable;
 
-export type BikesharingMember = 'yes' | 'no' | 'dontKnow' | 'nonApplicable';
+export type BikesharingMember = YesNoDontKnowNonApplicable;
 
 // Be careful how the question is asked. Used bikesharing service in the previous 30 days, previous 6 months?
 // This should denote whether a person can use bikesharing service/knows how to use it:
-export type BikesharingUser = 'yes' | 'no' | 'dontKnow' | 'nonApplicable';
+export type BikesharingUser = YesNoDontKnowNonApplicable;
 
-export type RidesharingMember = 'yes' | 'no' | 'dontKnow' | 'nonApplicable';
+export type RidesharingMember = YesNoDontKnowNonApplicable;
 
 // Be careful how the question is asked. Used ridesharing service in the previous 30 days, previous 6 months?
 // This should denote whether a person can use ridesharing service/knows how to use it:
-export type RidesharingUser = 'yes' | 'no' | 'dontKnow' | 'nonApplicable';
+export type RidesharingUser = YesNoDontKnowNonApplicable;
 
 // A person can have multiple occupations, like fullTimeStudent + partTimeWorker:
-export type Occupation =
-    | 'fullTimeWorker'
-    | 'partTimeWorker'
-    | 'fullTimeStudent'
-    | 'partTimeStudent'
-    | 'retired'
-    | 'atHome'
-    | 'other'
-    | 'nonApplicable';
+export const occupationValues = [
+    'fullTimeWorker',
+    'partTimeWorker',
+    'fullTimeStudent',
+    'partTimeStudent',
+    'workerAndStudent',
+    'retired',
+    'atHome',
+    'unemployed',
+    'other',
+    'nonApplicable',
+    'preferNotToAnswer',
+];
+export type Occupation = typeof occupationValues[number];
 
 export type JobCategory = string | 'dontKnow' | 'nonApplicable'; // TODO: add job categories from official source if possible and document it
 export type JobName = string;
-export type IsOnTheRoadWorker = 'yes' | 'no' | 'dontKnow' | 'nonApplicable';
+export type IsOnTheRoadWorker = YesNoDontKnowNonApplicable;
 
 // Whether the job is compatible with telecommute:
-export type IsJobTelecommuteCompatible = 'yes' | 'no' | 'dontKnow' | 'nonApplicable';
+export type IsJobTelecommuteCompatible = YesNoDontKnowNonApplicable;
 
 export type EducationalAttainment = string | 'dontKnow' | 'nonApplicable'; // TODO: add educational attainments from official source if possible and document it
