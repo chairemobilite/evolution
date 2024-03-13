c9107811e619c1047cd4321ecaf8cc87f7726cf6
Samuel Duhaime-Morissette <78976679+samuel-duhaime@users.noreply.github.com>
2024-03-13 10:31:17 -0400

    Add sectionsTypes and new inputTypes (#398)
    * Add sectionsTypes and new inputTypes
    
    * Add better types for inputTypes.ts

---
+++ packages/evolution-generator/src/types/sectionsTypes.ts
@@ -0,0 +1,42 @@
+// Config for the section
+export type SectionConfig = {
+    previousSection: string | null;
+    nextSection: string | null;
+    hiddenInNav?: boolean;
+    title?: {
+        fr: string;
+        en: string;
+    };
+    menuName?: {
+        fr: string;
+        en: string;
+    };
+    parentSection?: string;
+    widgets: string[];
+    groups?: {
+        [groupName: string]: {
+            showGroupedObjectDeleteButton: (interview: any, path: string) => boolean;
+            showGroupedObjectAddButton: (interview: any, path: string) => boolean;
+            groupedObjectAddButtonLabel: {
+                fr: string;
+                en: string;
+            };
+            addButtonSize: string;
+            widgets: string[];
+        };
+    };
+    preload?: (
+        interview: any,
+        startUpdateInterview: any,
+        startAddGroupedObjects: any,
+        startRemoveGroupedObjects: any,
+        callback: any
+    ) => null;
+    enableConditional: ((interview: any) => boolean) | boolean;
+    completionConditional: ((interview: any) => boolean) | boolean;
+};
+
+// Configs for the sections configs
+export type SectionsConfigs = {
+    [sectionName: string]: SectionConfig;
+};
