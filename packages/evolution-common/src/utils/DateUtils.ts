291f5a687d21d492ea857472a6e6a54d337dada9
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2024-05-22 12:25:22 -0400

    add required Utils for refactoring

--- packages/evolution-common/src/utils/DateUtils.ts
+++ packages/evolution-common/src/utils/DateUtils.ts
@@ -4,11 +4,16 @@
  * This file is licensed under the MIT License.
  * License text available at https://opensource.org/licenses/MIT
  */
-/**
- * This file provides a router with the routes to validate interviews
- * */
 
-// can return invalid dates: use isNaN on date.getTime() to check if it's valid
+import { Optional } from '../types/Optional.type';
+
+/**
+ * can return invalid dates: use isNaN on date.getTime() to check if it's valid
+ * @deprecated use parseDate instead
+ * @param date the date string or date object
+ * @param showErrorOnValidate whether to show an error in the console if the date is invalid
+ * @returns date object | undefined if invalid or undefined or null
+ */
 export const parseDate = function (
     date: string | Date | undefined | null,
     showErrorOnValidate = false
@@ -24,4 +29,72 @@ export const parseDate = function (
         console.error('DateUtils parseDate: invalid date');
     }
     return parsedDate;
+};
+
+/**
+ * Parse a date string and returns a Date object, checking validity.
+ * @param dateString the date in string or Date format
+ * @param showErrorOnValidate Whether to show an error in the console if the date is invalid
+ * @returns Date or undefined if invalid
+ */
+export const getDateFromDateString = function (
+    dateString: Optional<string>,
+    showErrorOnValidate = false
+): Optional<Date> {
+    if (dateString === undefined || dateString === '' || dateString === null) {
+        return undefined;
+    }
+    const parsedDate = new Date(dateString);
+    if (isNaN(parsedDate.getDate())) {
+        if (showErrorOnValidate) {
+            console.error('DateUtils getDateFromDateString: invalid date string');
+        }
+        return undefined;
+    }
+    return parsedDate;
+};
+
+/**
+ * Convert a date to a string with the format 'YYYY-MM-DD' (ignore timezone)
+ * Does not convert from/ot utc time.
+ * Make sure to use getUTCDateFromLocalDate to get the correct UTC date
+ * ALWAYS use this function to get the date string from a date to avoid timezone issues
+ * @param date Date object
+ * @returns a new Date object with the same date and time as the input date, but in local time
+ * or undefined if undefined or invalid date
+ */
+export const getDateStringFromDate = function (date: Optional<Date>): Optional<string> {
+    if (date === undefined || isNaN(date.getDate())) {
+        return undefined;
+    }
+    return [
+        date.getUTCFullYear(),
+        ('00' + (date.getUTCMonth() + 1).toString()).slice(-2),
+        ('00' + date.getUTCDate().toString()).slice(-2)
+    ].join('-');
+};
+
+/**
+ * Get weekday from date (ignore timezone)
+ * @param date Date object
+ * @returns a number from 0 to 6 (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+ * or undefined if undefined or invalid date
+ */
+export const getWeekdayFromDate = function (date: Optional<Date>): Optional<number> {
+    if (date === undefined || isNaN(date.getDate())) {
+        return undefined;
+    }
+    return date.getUTCDay();
+};
+
+/**
+ * Convert a date to a unix/epoch timestamp in seconds.
+ * @param date Date object
+ * @returns a timestamp in seconds or undefined if undefined or invalid date
+ */
+export const getUnixEpochFromDate = function (date: Optional<Date>): Optional<number> {
+    if (date === undefined || isNaN(date.getDate())) {
+        return undefined;
+    }
+    return date.valueOf() / 1000;
 };
--- packages/evolution-common/src/utils/__tests__/DateUtils.test.ts
+++ packages/evolution-common/src/utils/__tests__/DateUtils.test.ts
@@ -5,8 +5,15 @@
  * License text available at https://opensource.org/licenses/MIT
  */
 
-import { parseDate } from '../DateUtils';
+import {
+    parseDate,
+    getDateFromDateString,
+    getDateStringFromDate,
+    getWeekdayFromDate,
+    getUnixEpochFromDate,
+} from '../DateUtils';
 
+// deprecated:
 describe('parseDate function', () => {
     // Test cases for undefined input and Date object input
     test.each([
@@ -14,7 +21,7 @@ describe('parseDate function', () => {
         ['', undefined, 'returns undefined for empty string input'],
         [null, undefined, 'returns undefined for null input'],
         [new Date('2020-01-01'), new Date('2020-01-01'), 'returns the same Date object for Date input'],
-    ])('%s: %s', (input, expected, description) => {
+    ])('%s: %s', (input, expected, _) => {
         expect(parseDate(input)).toEqual(expected);
     });
 
@@ -22,7 +29,7 @@ describe('parseDate function', () => {
     test.each([
         ['2020-01-01', true, 'returns a valid Date object for valid date string input'],
         ['invalid-date', false, 'returns an invalid Date object for invalid date string input'],
-    ])('%s: %s', (input, isValid, description) => {
+    ])('%s: %s', (input, isValid, _) => {
         const result = parseDate(input);
         expect(result instanceof Date).toBeTruthy();
         expect(isValid ? !isNaN((result as Date).getTime()) : isNaN((result as Date).getTime())).toBeTruthy();
@@ -31,7 +38,7 @@ describe('parseDate function', () => {
     // Test case for logging error on invalid date string input
     test.each([
         ['invalid-date', true, 'logs an error for invalid date string input when showErrorOnValidate is true'],
-    ])('%s with showErrorOnValidate=%s: %s', (input, showErrorOnValidate, description) => {
+    ])('%s with showErrorOnValidate=%s: %s', (input, showErrorOnValidate, _) => {
         const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
         parseDate(input, showErrorOnValidate);
         if (showErrorOnValidate) {
@@ -40,5 +47,83 @@ describe('parseDate function', () => {
             expect(consoleSpy).not.toHaveBeenCalled();
         }
         consoleSpy.mockRestore();
+    });
+});
+
+describe('getDateFromDateString', () => {
+    test('returns undefined for undefined, empty string, or null input', () => {
+        expect(getDateFromDateString(undefined)).toBeUndefined();
+        expect(getDateFromDateString('')).toBeUndefined();
+    });
+
+    test('returns undefined for invalid date string', () => {
+        const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
+
+        expect(getDateFromDateString('invalid')).toBeUndefined();
+        expect(consoleErrorSpy).not.toHaveBeenCalled();
+
+        expect(getDateFromDateString('invalid', true)).toBeUndefined();
+        expect(consoleErrorSpy).toHaveBeenCalledWith('DateUtils getDateFromDateString: invalid date string');
+
+        consoleErrorSpy.mockRestore();
+    });
+
+    test('returns a valid Date object for valid date string', () => {
+        const dateString = '2023-05-22';
+        const result = getDateFromDateString(dateString);
+        expect(result).toBeInstanceOf(Date);
+        expect(result?.toISOString()).toEqual('2023-05-22T00:00:00.000Z');
+    });
+});
+
+describe('getDateStringFromDate', () => {
+    test('returns undefined for undefined or invalid date input', () => {
+        expect(getDateStringFromDate(undefined)).toBeUndefined();
+        expect(getDateStringFromDate(new Date('invalid'))).toBeUndefined();
+    });
+
+    test('returns a valid date string in the format YYYY-MM-DD for valid date input', () => {
+        const date = new Date('2023-05-22T00:00:00.000Z');
+        const result = getDateStringFromDate(date);
+        expect(result).toEqual('2023-05-22');
+    });
+
+    test('returns a valid date string in the format YYYY-MM-DD for valid date input (with needed padding)', () => {
+        const date = new Date('2023-01-01T00:00:00.000Z');
+        const result = getDateStringFromDate(date);
+        expect(result).toEqual('2023-01-01');
+    });
+
+    test('returns a valid date string in the format YYYY-MM-DD for valid date input (with different hours)', () => {
+        const date = new Date('2023-01-01T23:59:00.000Z');
+        const result = getDateStringFromDate(date);
+        expect(result).toEqual('2023-01-01');
+    });
+
+});
+
+describe('getWeekdayFromDate', () => {
+    test('returns undefined for undefined or invalid date input', () => {
+        expect(getWeekdayFromDate(undefined)).toBeUndefined();
+        expect(getWeekdayFromDate(new Date('invalid'))).toBeUndefined();
+    });
+
+    test('returns the correct weekday number for valid date input', () => {
+        const date = new Date('2023-05-22T00:00:00.000Z'); // Monday
+        const result = getWeekdayFromDate(date);
+        expect(result).toEqual(1);
+    });
+});
+
+describe('getUnixEpochFromDate', () => {
+    test('returns undefined for undefined or invalid date input', () => {
+        expect(getUnixEpochFromDate(undefined)).toBeUndefined();
+        expect(getUnixEpochFromDate(new Date('invalid'))).toBeUndefined();
+    });
+
+    test('returns a valid Unix epoch timestamp in seconds for valid date input', () => {
+        const date = new Date('2023-05-22T00:00:00.000Z');
+        const result = getUnixEpochFromDate(date);
+        expect(result).toEqual(1684713600);
     });
 });
