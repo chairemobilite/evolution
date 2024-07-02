60afa02fd2f1ff8d6ba5b59f29274251d7973330
samuel-duhaime <sduhaimemorissette@gmail.com>
2024-07-02 16:53:43 -0400

    Add registerWithEmailTest UI test
    Add registerWithEmailTest UI test
    Generate start the survey template UI test
    Format files

--- packages/evolution-frontend/tests/ui-testing/surveyTestHelpers.ts
+++ packages/evolution-frontend/tests/ui-testing/surveyTestHelpers.ts
@@ -11,24 +11,58 @@ import * as testHelpers from './testHelpers';
  * opened
  * @param {string} title - The title of the survey
  */
-export const startAndLoginAnonymously = ({ context, title, hasUser = true }: { title: string, hasUser: boolean } & testHelpers.CommonTestParameters) => {
+export const startAndLoginAnonymously = ({
+    context,
+    title,
+    hasUser = true
+}: { title: string; hasUser: boolean } & testHelpers.CommonTestParameters) => {
     // Test the survey landing page
     testHelpers.hasTitleTest({ title, context });
     testHelpers.hasFrenchTest({ context });
     testHelpers.switchToEnglishTest({ context });
     testHelpers.hasConsentTest({ context });
     testHelpers.startSurveyTest({ context });
-    
+
     // Test the login page
     testHelpers.registerWithoutEmailTest({ context });
-    
+
     // Test the home section page
     if (hasUser) {
         testHelpers.hasUserTest({ context });
     }
-}
+};
+
+/**
+ * Starts a survey and logs in with an email.
+ *
+ * This function performs a series of tests on the survey landing page, including checking the title,
+ * switching languages, and consenting to the survey. After navigating through the landing page,
+ * it tests the login functionality by registering with an email.
+ *
+ * @param {Object} params - The parameters for starting and logging in.
+ * @param {testHelpers.CommonTestParameters} params.context - The test context.
+ * @param {string} params.title - The expected title of the survey landing page.
+ * @param {string} params.email - The email address to use for registration.
+ * @param {string} params.nextPageUrl - The URL of the page to navigate to after login.
+ */
+export const startAndLoginWithEmail = ({
+    context,
+    title,
+    email,
+    nextPageUrl
+}: { title: string; email: string; nextPageUrl: string } & testHelpers.CommonTestParameters) => {
+    // Test the survey landing page
+    testHelpers.hasTitleTest({ title, context });
+    testHelpers.hasFrenchTest({ context });
+    testHelpers.switchToEnglishTest({ context });
+    testHelpers.hasConsentTest({ context });
+    testHelpers.startSurveyTest({ context });
+
+    // Test the login page
+    testHelpers.registerWithEmailTest({ context, email, nextPageUrl });
+};
 
 export const logout = ({ context }: testHelpers.CommonTestParameters) => {
     // Test the survey logout page
     testHelpers.logoutTest({ context });
-}
+};
