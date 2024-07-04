6f786cec189ca615c2eb73498a6129022535ca31
Geneviève Bastien <gbastien@versatic.net>
2024-07-04 12:10:54 -0400

    server: Add a deferred server update callback mechanism
    fixes #582
    
    Add an extra parameter for server update callbacks, so that callbacks
    that have time-consuming operations (like routing calculations) can
    register the operation, which can then execute without blocking the
    current client update call.
    
    The callbacks can return quickly empty or other values, but register
    an operation, identified by a `opName` and `opUniqueId`. The `opName`
    ensures that only one operation for that name will be considered. The
    `opUniqueId` allows to cancel any previous operation with the same name.
    The `operation` to register is a function that receives a `isCancelled`
    function and returns a promise that will run in the background. The
    promise should return the values by path to set in the interview. The
    interview will be saved with the new values and those can be sent back
    to the client at the next update call.
    
    The deferred update callback will save the updated values in the user's
    session and they will be added to the returned server values in the next
    client response.
    
    Example usage of this mechanism
    
    ```
    field: 'accessCode',
    callback: (interview, value, path, registerUpdateOperation) => {
        // Use the update operation to set the city to Calgary, but 5 seconds later
        registerUpdateOperation({
            opName: 'city',
            opUniqueId: 1,
            operation: () => new Promise((resolve) => {
                setTimeout(() => {
                    resolve({
                        'home.city': 'Calgary'
                    });
                }, 5000);
            })
        });
    }
    ```

--- packages/evolution-backend/src/services/interviews/serverFieldUpdate.ts
+++ packages/evolution-backend/src/services/interviews/serverFieldUpdate.ts
@@ -20,13 +20,60 @@ type FieldUpdateCallbackReturnType =
 
 /**
  * Type for callbacks when a response field gets updated.
+ *
+ * @param {Object} args The arguments to pass to the registration call
+ * @param {string} args.opName The name of the operation to register
+ * @param {unknown} args.opUniqueId The unique identifier of the operation. It
+ * should be such that when comparing with `===` returns true, it is the same
+ * operation. Otherwise, it is a different one.
+ * @param {Function} args.operation  The operation to register is a function
+ *     that receives a `isCancelled` callback to verify the cancellation status
+ *     of the operation. The execution itself should be done in this function.
+ *     It should return an object where the keys are the path in the responses
+ *     field and the value is the value to update.
+ *
+ *     It is the responsibility of the operation to check if it is cancelled once
+ *     in a while. A good practice is to check it at least every 100ms, or at
+ *     least every 1000 iterations of a loop. If a cancelled operation
+ *     terminates, its values will be ignored.
+ */
+type RegisterUpdateOperationType = (args: {
+    opName: string;
+    opUniqueId: unknown;
+    operation: (isCancelled: () => boolean) => Promise<FieldUpdateCallbackResponseCallbackType>;
+}) => Promise<void>;
+
+/**
+ * Type for callbacks when a response field gets updated.
  */
 export type ServerFieldUpdateCallback = {
     field: string | { regex: string };
+    /**
+     *
+     * @param interview The interview to update
+     * @param newValue The new value to set for the field, as received from the
+       client
+     * @param path The path of the value to update in the interview
+     * @param registerUpdateOperation A function that can optionally be called
+     * to register a long-running, time-consuming operations. This will run the
+     * operation in the background and will not block the current update call.
+     * When the response is obtained, it will automatically notify the interview
+     * to save the responses data and send it back to the client later, if
+     * necessary. To avoid many operations running at the same time for a given
+     * field, the registration function takes a name to associate with, and a
+     * unique operation identifier. If an operation with the same name is
+     * registered again with the same identifier, it will be ignored. Otherwise,
+     * it will cancel the previous operation and start the new one.
+     *
+     * Not that a single server update callback can register multiple
+     * operations, if they are under different names.
+     * @returns
+     */
     callback: (
         interview: InterviewAttributes,
         newValue: unknown | undefined,
-        path: string
+        path: string,
+        registerUpdateOperation?: RegisterUpdateOperationType
     ) => Promise<FieldUpdateCallbackReturnType>;
 };
 
@@ -67,6 +114,68 @@ const getUpdateCallbackForPath = (
     return serverCallback !== undefined ? [responsePath, serverCallback] : undefined;
 };
 
+class InterviewUpdateOperation {
+    private static runningQueries: { [interviewId: string]: { [opName: string]: unknown } } = {};
+
+    private static getCurrentOperation = (opName: string, interviewId: string) => {
+        return (InterviewUpdateOperation.runningQueries[interviewId] || {})[opName];
+    };
+
+    constructor(
+        private interviewUuid: string,
+        private deferredUpdateCallback: (valuesByPath: { [key: string]: unknown }) => Promise<void>
+    ) {
+        // Nothing else to do
+    }
+
+    public registerOperation = async (args: {
+        opName: string;
+        opUniqueId: unknown;
+        operation: (isCancelled: () => boolean) => Promise<{ [path: string]: unknown }>;
+    }): Promise<void> => {
+        const currentOp = InterviewUpdateOperation.getCurrentOperation(args.opName, this.interviewUuid);
+        if (currentOp !== undefined && currentOp === args.opUniqueId) {
+            // The same operation is already running, ignore
+            return;
+        }
+        // Register the current operation
+        InterviewUpdateOperation.runningQueries[this.interviewUuid] = {
+            ...(InterviewUpdateOperation.runningQueries[this.interviewUuid] || {}),
+            [args.opName]: args.opUniqueId
+        };
+        const isCancelled = () =>
+            InterviewUpdateOperation.getCurrentOperation(args.opName, this.interviewUuid) !== args.opUniqueId;
+        try {
+            const responsesByPath = await args.operation(isCancelled);
+            if (isCancelled()) {
+                return;
+            }
+
+            // Convert the responses to the format expected by the deferred callback
+            const asyncServerValuesByPath = {};
+            Object.keys(responsesByPath).forEach((key) => {
+                asyncServerValuesByPath[`responses.${key}`] = responsesByPath[key];
+            });
+
+            // Call the update callback
+            this.deferredUpdateCallback(asyncServerValuesByPath);
+        } catch (error) {
+            console.log(
+                'error while running server update callback operation for interview %s, name %s, opId %s: %s',
+                this.interviewUuid,
+                args.opName,
+                args.opUniqueId,
+                error
+            );
+        } finally {
+            // Reset the callback for this name
+            if (!isCancelled()) {
+                delete InterviewUpdateOperation.runningQueries[this.interviewUuid][args.opName];
+            }
+        }
+    };
+}
+
 /**
  * Get the values by path that should be updated, given the current values by
  * path.
@@ -82,13 +191,19 @@ const getUpdateCallbackForPath = (
  * callbacks to call when fields are updated
  * @param {{ [key: string]: any }} valuesByPath The valuesByPath to update
  * @param {string[]} unsetValues The valuesbyPath to set to undefined
+ * @param {((valuesByPath: { [key: string]: any }) => Promise<void> |
+ * undefined)} deferredUpdateCallback A callback passed to the server update
+ * callbacks, that can be optionally called to send the response to the client.
+ * It should be used by update callbacks that can take a lot of time to execute
+ * instead of blocking the current update call.
  * @return {*}  {Promise<{ [key: string]: any }>}
  */
 const updateFields = async (
     interview: InterviewAttributes,
     serverUpdateCallbacks: ServerFieldUpdateCallback[],
     valuesByPath: { [key: string]: unknown },
-    unsetValues?: string[]
+    unsetValues?: string[],
+    deferredUpdateCallback?: (valuesByPath: { [key: string]: unknown }) => Promise<void>
 ): Promise<[{ [key: string]: any }, string | undefined]> => {
     if (serverUpdateCallbacks.length === 0) {
         return [{}, undefined];
@@ -100,11 +215,18 @@ const updateFields = async (
     const callbacks = Object.keys(valuesByPath)
         .map((path) => getUpdateCallbackForPath(serverUpdateCallbacks, path))
         .filter((callbackPair) => callbackPair !== undefined);
+
+    // Map callback results to responses before calling the asynchronous execution callback is specified
+    const deferredCallback =
+        deferredUpdateCallback !== undefined
+            ? new InterviewUpdateOperation(interview.uuid, deferredUpdateCallback).registerOperation
+            : undefined;
+
     for (let i = 0; i < callbacks.length; i++) {
         const [path, serverCallback] = callbacks[i] as [string, ServerFieldUpdateCallback];
         const [updatedValuesByPath, callbackUrl] = await waitExecuteCallback(
             interview,
-            serverCallback.callback(interview, valuesByPath[`responses.${path}`], path),
+            serverCallback.callback(interview, valuesByPath[`responses.${path}`], path, deferredCallback),
             path
         );
         Object.assign(serverValuesByPath, updatedValuesByPath);
@@ -120,7 +242,7 @@ const updateFields = async (
             const [path, serverCallback] = callbacks[i] as [string, ServerFieldUpdateCallback];
             const [updatedValuesByPath, callbackUrl] = await waitExecuteCallback(
                 interview,
-                serverCallback.callback(interview, undefined, path),
+                serverCallback.callback(interview, undefined, path, deferredCallback),
                 path
             );
             Object.assign(serverValuesByPath, updatedValuesByPath);
