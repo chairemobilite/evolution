979499a075715483119f9e1ef3d5ebfa76d8d8dd
Geneviève Bastien <gbastien@versatic.net>
2024-07-22 12:26:31 -0400

    InfoMap: Move the configuration getter in googleMaps.config
    fixes #607
    
    All calls to `useJsLoaderApi` need to be done with the same parameters
    in all components using it. So that configuration is moved to the
    googleMaps.config file directly and it exports a configuration getter
    instead of the configuration itself.

--- packages/evolution-frontend/src/config/googleMaps.config.ts
+++ packages/evolution-frontend/src/config/googleMaps.config.ts
@@ -4,16 +4,37 @@
  * This file is licensed under the MIT License.
  * License text available at https://opensource.org/licenses/MIT
  */
+import { useJsApiLoader } from '@react-google-maps/api';
 import InputLoading from '../components/inputs/InputLoading';
+import projectConfig from 'chaire-lib-common/lib/config/shared/project.config';
 
-export const googleMapConfigNew = {
+const googleMapConfigNew = {
     id: 'google-map-script',
     googleMapsApiKey: process.env.GOOGLE_API_KEY as string,
     libraries: ['places' as const, 'geometry' as const]
 };
 
+// Legacy google map configuration, still used in old PhotonOsmInputMap component
 export default {
     apiKey: process.env.GOOGLE_API_KEY as string,
     LoadingContainer: InputLoading,
     libraries: ['places', 'geometry']
+};
+
+// The google map configuration needs to be global as the loading of the API
+// takes place once for the whole survey and the configuration cannot change,
+// even between sections, otherwise it throws an exception.
+let currentGoogleMapConfig: Parameters<typeof useJsApiLoader>[0] | undefined = undefined;
+export const getCurrentGoogleMapConfig = (
+    language = projectConfig.defaultLocale
+): Parameters<typeof useJsApiLoader>[0] => {
+    if (currentGoogleMapConfig) {
+        return currentGoogleMapConfig;
+    }
+    currentGoogleMapConfig = {
+        region: projectConfig.region,
+        language: language,
+        ...googleMapConfigNew
+    };
+    return currentGoogleMapConfig;
 };
