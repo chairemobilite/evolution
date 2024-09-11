ed80b93109f10d21162e2ba4014be4a9b58f9c41
Gabriel Bruno <gabriel.bruno@polymtl.ca>
2024-09-11 15:10:57 -0400

    The github workflow now runs the playwright ui tests and adds a secret google api key. Also fixes a broken test that wasn't caught by the pipeline because it does not run it. Fix: #681

--- example/demo_survey/tests/test-two-adults-no-trips.UI.spec.ts
+++ example/demo_survey/tests/test-two-adults-no-trips.UI.spec.ts
@@ -31,6 +31,7 @@ testHelpers.inputStringTest({ context, path: 'home.country', value: 'Canada' });
 testHelpers.inputStringTest({ context, path: 'home.postalCode', value: 'H1S1V7' });
 //testHelpers.inputMapFindPlaceTest({ context, path: 'home.geography' });
 testHelpers.inputSelectTest({ context, path: 'home.dwellingType', value: 'tenantSingleDetachedHouse' });
+testHelpers.waitForMapToBeLoaded({ context });
 testHelpers.inputNextButtonTest({ context, text: 'Save and continue', nextPageUrl: '/survey/householdMembers' });
 
 // Test the household page
