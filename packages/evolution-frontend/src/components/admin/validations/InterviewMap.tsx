f013188dc2ab3b229c09e6a1b88eb2bb396ebed9
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2024-09-24 09:18:58 -0400

    admin: Use deck.gl for the InterviewMap component
    fixes #668
    
    Add dependencies to `deck.gl` packages and `popmotion` for arrow
    animation.
    
    Add the mapDefaultCenter to the project's configuration type.
    
    Change the InterviewMap component to use deck.gl instead. Add an
    AnimatedArrowPathLayer to display of path layer with moving arrows
    showing the direction of trips.
    
    Add admin styles from the od_nationale_2024 survey for proper layout of
    the admin interface.
    
    ** From an original commit in a survey by Pierre-Léo Bourbonnais, ported
      to evolution by Geneviève Bastien **
    
    Co-authored-by: Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
    Co-authored-by: Geneviève Bastien <gbastien@versatic.net>

--- packages/evolution-frontend/src/components/admin/validations/InterviewMap.tsx
+++ packages/evolution-frontend/src/components/admin/validations/InterviewMap.tsx
@@ -4,321 +4,158 @@
  * This file is licensed under the MIT License.
  * License text available at https://opensource.org/licenses/MIT
  */
-import React from 'react';
+import React, { useState } from 'react';
+import config from 'evolution-common/lib/config/project.config';
 import { withTranslation } from 'react-i18next';
-import ReactMapboxGl, { Popup, Layer, Feature, GeoJSONLayer } from 'react-mapbox-gl';
-import {
-    bbox as turfBbox,
-    length as turfLength,
-    along as turfAlong,
-    bearing as turfBearing,
-    lineString as turfLineString
-} from '@turf/turf';
+import DeckGL from '@deck.gl/react';
+import { WebMercatorViewport, MapViewState } from '@deck.gl/core';
+import { TileLayer } from '@deck.gl/geo-layers';
+import { IconLayer, BitmapLayer } from '@deck.gl/layers';
+import { AnimatedArrowPathLayer } from './AnimatedArrowPathLayer';
+import * as VPAttr from 'evolution-common/lib/services/baseObjects/attributeTypes/VisitedPlaceAttributes';
+import { animate, linear } from 'popmotion';
 
-const Map = ReactMapboxGl({
-    accessToken: process.env.MAPBOX_ACCESS_TOKEN as string
-});
+export type SurveyMapPointProperties = {
+    activity: VPAttr.Activity;
+    active?: string;
+};
 
-class InterviewMap extends React.Component<any, any> {
-    private defaultZoomArray: any;
-    private defaultCenter: any;
-    private map: any;
-    private ref: any;
-    private resizeObserver: ResizeObserver;
+export type SurveyMapTripProperties = {
+    active?: string;
+    color?: string;
+};
 
-    constructor(props) {
-        super(props);
+type InterviewMapProps = {
+    center?: [number, number];
+    updateCount?: number;
+    places: GeoJSON.FeatureCollection<GeoJSON.Point, SurveyMapPointProperties>;
+    trips: GeoJSON.FeatureCollection<GeoJSON.LineString, SurveyMapTripProperties>;
+};
 
-        this.defaultZoomArray = [props.zoom || 13];
-        this.defaultCenter = props.center || [-73.6, 45.5];
+const InterviewMap: React.FunctionComponent<InterviewMapProps> = (props: InterviewMapProps) => {
+    const [viewState, setViewState] = useState<MapViewState>({
+        longitude: props.center ? props.center[0] : config.mapDefaultCenter.lon,
+        latitude: props.center ? props.center[1] : config.mapDefaultCenter.lat,
+        zoom: 13
+    });
 
-        this.state = {
-            dragging: false
-        };
-        this.map = null;
-        this.ref = React.createRef();
-        //this.onZoomEnd        = this.onZoomEnd.bind(this);
-        //this.onDragEnd        = this.onDragEnd.bind(this);
-        this.handleMapResize = this.handleMapResize.bind(this);
-        this.handleResize = this.handleResize.bind(this);
-        //this.handleMapClick   = this.handleMapClick.bind(this);
-        this.onFeatureDragEnd = this.onFeatureDragEnd.bind(this);
-        this.onFeatureDragStart = this.onFeatureDragStart.bind(this);
-        this.onFeatureClick = this.onFeatureClick.bind(this);
-        this.onShapeClick = this.onShapeClick.bind(this);
-        this.resizeObserver = new ResizeObserver((entries) => {
-            this.handleResize();
+    const [zoom, setZoom] = useState<number>(13);
+
+    const loopLength = 10000;
+    const animationSpeed = 100;
+    const [currentTime, setCurrentTime] = useState<number>(0);
+
+    React.useEffect(() => {
+        const animation = animate({
+            from: 0,
+            to: loopLength,
+            duration: loopLength,
+            repeat: Infinity,
+            ease: linear,
+            onUpdate: setCurrentTime
         });
-    }
+        return () => animation.stop();
+    }, [loopLength, animationSpeed]);
 
-    onShapeClick(e) {
-        this.props.selectTrip(e.feature.properties.tripUuid);
-    }
+    const [hasLoaded, setHasLoaded] = useState<boolean>(false);
 
-    onZoomEnd(map, event) {
-        return null;
-    }
+    const tileLayer = new TileLayer({
+        id: 'TileLayer',
+        data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        maxZoom: 20,
+        minZoom: 0,
+        tileSize: 256,
+        opacity: 0.5,
+        renderSubLayers: (props) => {
+            const { boundingBox } = props.tile;
 
-    onDragEnd(map, event) {
-        return null;
-        //const centerLatLng = event.target.getCenter();
-        //console.log(event.target.getCenter());
-    }
-
-    handleMapResize(map, bounds) {
-        //console.log('handleMapREsize');
-        if (!this.map) {
-            this.map = map;
-        }
-        if (bounds) {
-            map.fitBounds(bounds, { padding: 50, animate: false });
-        } // else: it means we have only the home coordinates, so ignore bounds
-        map.resize();
-    }
-
-    handleResize() {
-        //console.log('handleResize');
-
-        if (this.map) {
-            this.map.resize();
-        }
-    }
-
-    componentDidMount() {
-        if (this.ref.current) {
-            this.resizeObserver.observe(this.ref.current);
-        }
-    }
-
-    componentWillUnmount() {
-        if (this.ref.current) {
-            this.ref.current.removeEventListener('resize', this.handleResize);
-        }
-    }
-
-    onFeatureDragStart(e) {
-        if (this.props.user.isAuthorized({ Interviews: ['confirm'] })) {
-            this.setState({
-                dragging: true
-            });
-        }
-    }
-
-    onFeatureDragEnd(e, place) {
-        if (this.props.user.isAuthorized({ Interviews: ['confirm'] })) {
-            this.props.startUpdateInterview(
-                'validation',
+            return new BitmapLayer(
+                props as any,
                 {
-                    [place.path]: [e.lngLat.lng, e.lngLat.lat]
-                },
-                null,
-                null,
-                () => {
-                    this.setState({
-                        dragging: false
-                    });
-                    console.log('updated coordinates for path', place.path);
-                }
+                    data: null,
+                    image: props.data,
+                    bounds: [boundingBox[0][0], boundingBox[0][1], boundingBox[1][0], boundingBox[1][1]]
+                } as any
             );
-        }
-    }
+        },
+        pickable: true
+    });
 
-    onFeatureClick(e) {
-        this.props.selectPlace(e.feature.properties.path);
-    }
-
-    render() {
-        const markers: any[] = [];
-        const markerIcons: any[] = [];
-        const modeMarkers: any[] = [];
-        const modeIcons: any[] = [];
-        const tripShapes: any[] = [];
-        const arrowMarkers: any[] = [];
-        const arrowIcons: any[] = [];
-        const boundsCoordinates: any[] = [];
-        const popups: JSX.Element[] = [];
-        for (let i = 0, count = this.props.places.length; i < count; i++) {
-            const place = this.props.places[i];
-            if (place.activity) {
-                const icon = new Image(40, 40);
-                icon.src = `/dist/images/activities_icons/${place.activity}_marker.svg`;
-                markerIcons.push([place.path, icon]);
-
-                const marker = (
-                    <Feature
-                        key={place.path}
-                        coordinates={place.coordinates}
-                        properties={{ ...place }}
-                        draggable={this.props.user.isAuthorized({ Interviews: ['confirm'] })}
-                        onClick={this.onFeatureClick}
-                        onDragEnd={(e) => this.onFeatureDragEnd(e, place)}
-                        onDragStart={this.onFeatureDragStart}
-                    />
-                );
-                if (place.name && this.state.dragging === false) {
-                    const popup = (
-                        <Popup key={place.path} coordinates={place.coordinates} anchor="bottom" offset={[0, -22]}>
-                            <p>
-                                {place.name} ({place.lastAction || '?'})
-                            </p>
-                        </Popup>
-                    );
-                    popups.push(popup);
-                }
-                boundsCoordinates.push(place.coordinates);
-                markers.push(marker);
+    const placesLayer = new IconLayer({
+        id: 'places',
+        data: props.places.features,
+        getIcon: (d) => ({
+            url: `/dist/images/activities_icons/${d.properties.activity || 'default'}_marker.svg`,
+            width: 256,
+            height: 256,
+            anchorY: 256
+        }),
+        loadOptions: {
+            imagebitmap: {
+                resizeWidth: 256,
+                resizeHeight: 256,
+                resizeQuality: 'high'
             }
-        }
-        const arrowIcon = new Image(10, 10);
-        arrowIcon.src = '/dist/images/interface_icons/arrow-up-solid.svg';
-        arrowIcons.push(['arrow', arrowIcon]);
-        for (let i = 0, count = this.props.tripShapes.length; i < count; i++) {
-            const trip = this.props.tripShapes[i];
-            const tripShapeLength = turfLength(trip.geometry, { units: 'meters' });
-            const tripShapeMiddlePoint = turfAlong(trip.geometry, tripShapeLength / 2, { units: 'meters' });
-            const arrowBearing = turfBearing(
-                turfAlong(trip.geometry, 0, { units: 'meters' }),
-                turfAlong(trip.geometry, tripShapeLength, { units: 'meters' })
-            );
-            const tripShape = (
-                <Feature
-                    key={trip.properties.tripUuid}
-                    coordinates={trip.geometry.coordinates}
-                    properties={{ ...trip.properties }}
-                    onClick={this.onShapeClick}
-                />
-            );
-            tripShapes.push(tripShape);
-            const arrowMarker = (
-                <Feature
-                    key={trip.properties.tripUuid}
-                    coordinates={tripShapeMiddlePoint.geometry.coordinates}
-                    properties={{ bearing: arrowBearing }}
-                />
-            );
-            arrowMarkers.push(arrowMarker);
+        },
+        getPosition: (d) => d.geometry.coordinates,
+        getSize: (d) => (d.active === 'true' ? 80 : 40),
+        pickable: true
+        //onClick: (info) => props.selectPlace(info.object.path)
+    });
 
-            for (let j = 0, countJ = trip.properties.modes.length; j < countJ; j++) {
-                const mode = trip.properties.modes[j];
+    const animatedArrowPathLayer = new AnimatedArrowPathLayer({
+        id: 'tripCurves',
+        data: props.trips.features,
+        getPath: (d) => d.geometry.coordinates,
+        updateTriggers: {
+            getPath: props.updateCount || 1,
+            getColor: props.updateCount || 1
+        },
+        getDistanceBetweenArrows: 100.0,
+        widthMaxPixels: 20,
+        currentTime,
+        speedDivider: 3000,
+        disableAnimation: false,
+        jointRounded: true,
+        billboard: true,
+        zoom,
+        getWidth: (d) => (d.properties.active ? 16 : 12),
+        getColor: (d) => {
+            const hex = (d.properties.color || '#000000').replace('#', '').toLowerCase();
+            // convert to RGB:
+            return hex.match(/[0-9a-f]{2}/g).map((x) => parseInt(x, 16));
+        }
+    });
 
-                if (mode) {
-                    const modeIcon = new Image(25, 25);
-                    modeIcon.src = `/dist/images/modes_icons/${mode}.png`;
-                    modeIcons.push([trip.properties.segmentUuids[j], modeIcon]);
-                    const offsetStart = -(25 * countJ) / 2;
-                    const modeMarker = (
-                        <Feature
-                            key={trip.properties.tripUuid}
-                            coordinates={tripShapeMiddlePoint.geometry.coordinates}
-                            properties={{
-                                uuid: trip.properties.segmentUuids[j],
-                                bearing: arrowBearing,
-                                offset: [25, offsetStart + 25 * j]
-                            }}
-                        />
-                    );
-                    modeMarkers.push(modeMarker);
-                }
-            }
+    const onAfterRender = () => {
+        if (!hasLoaded && placesLayer.isLoaded && props.places.features.length > 1) {
+            setHasLoaded(true);
+
+            const viewport = placesLayer.context.viewport as WebMercatorViewport;
+            const { longitude, latitude, zoom } = viewport.fitBounds(
+                placesLayer.getBounds() as [[number, number], [number, number]],
+                { padding: { top: 50, bottom: 50, left: 50, right: 50 } }
+            );
+            setViewState({ longitude, latitude, zoom });
+            setZoom(zoom);
         }
-        let bounds: any = null;
-        try {
-            bounds = turfBbox(turfLineString(boundsCoordinates));
-        } catch {
-            bounds = null;
-        }
-        return (
-            <div className="admin__interview-map" ref={this.ref}>
-                <Map
-                    style={'mapbox://styles/mapbox/streets-v9'}
-                    center={
-                        this.props.places && this.props.places[0] && this.props.places[0].coordinates
-                            ? this.props.places[0].coordinates
-                            : this.defaultCenter
-                    } // use constant otherwise the center will get back to default after state changes
-                    zoom={this.defaultZoomArray}
-                    onDragEnd={this.onDragEnd}
-                    onStyleLoad={(e) => {
-                        if (bounds) {
-                            this.handleMapResize(e, [
-                                [bounds[0], bounds[1]],
-                                [bounds[2], bounds[3]]
-                            ]);
-                        }
-                    }}
-                    // onClick          = {this.handleMapClick} // handleMapClick does not exist
-                    containerStyle={{
-                        height: '100%',
-                        width: '100%'
-                    }}
-                >
-                    {...popups as any}
-                    {tripShapes.length > 0 && (
-                        <Layer
-                            id="tripShapes"
-                            key="tripShapes"
-                            type="line"
-                            paint={{
-                                'line-color': ['get', 'color'],
-                                'line-width': ['match', ['get', 'active'], 'true', 10, 'false', 5, 5]
-                            }}
-                        >
-                            {tripShapes}
-                        </Layer>
-                    )}
-                    {arrowMarkers.length > 0 && (
-                        <Layer
-                            id="arrows"
-                            key="arrows"
-                            type="symbol"
-                            layout={{
-                                'icon-image': 'arrow',
-                                'icon-rotate': ['get', 'bearing'],
-                                'icon-allow-overlap': true,
-                                'icon-offset': [0, 0],
-                                'icon-size': 1.0
-                            }}
-                            images={arrowIcons}
-                        >
-                            {arrowMarkers}
-                        </Layer>
-                    )}
-                    {modeMarkers.length > 0 && (
-                        <Layer
-                            id="modes"
-                            key="modes"
-                            type="symbol"
-                            layout={{
-                                'icon-image': ['get', 'uuid'],
-                                'icon-allow-overlap': true,
-                                'icon-offset': ['get', 'offset'],
-                                'icon-size': ['interpolate', ['linear'], ['zoom'], 10, 0.35, 15, 1.0]
-                            }}
-                            images={modeIcons}
-                        >
-                            {modeMarkers}
-                        </Layer>
-                    )}
-                    {markers.length > 0 && (
-                        <Layer
-                            id="places"
-                            key="places"
-                            type="symbol"
-                            layout={{
-                                'icon-image': ['get', 'path'],
-                                'icon-allow-overlap': true,
-                                'icon-offset': [0, -20],
-                                'icon-size': ['match', ['get', 'active'], 'true', 1.0, 'false', 0.7, 0.7]
-                            }}
-                            images={markerIcons}
-                        >
-                            {markers}
-                        </Layer>
-                    )}
-                </Map>
-            </div>
-        );
-    }
-}
+    };
+
+    const layers = [tileLayer, animatedArrowPathLayer, placesLayer];
+
+    return (
+        <div className="admin__interview-map">
+            <DeckGL
+                initialViewState={viewState}
+                controller={true}
+                layers={layers}
+                onAfterRender={onAfterRender}
+                onViewStateChange={(params) => {
+                    setZoom((params.viewState as any).zoom);
+                }}
+            ></DeckGL>
+        </div>
+    );
+};
 
 export default withTranslation()(InterviewMap);
