62625ece78290a40be6cd45146898be6f43797f1
Geneviève Bastien <gbastien@versatic.net>
2024-10-04 16:55:35 -0400

    segments: Add missing modes in SegmentAttributes
    `scooterElectric`, `wheelchair` and `mobilityScooter` were recently
    added.
    
    Also, additional transit modes are added to cover all possible modes in
    the world, not just those available in Quebec province.
    
    Add the BicycleType type to fine-tune the bicycle main mode, similar to
    the CarType.

--- packages/evolution-common/src/services/baseObjects/attributeTypes/SegmentAttributes.ts
+++ packages/evolution-common/src/services/baseObjects/attributeTypes/SegmentAttributes.ts
@@ -5,36 +5,46 @@
  * License text available at https://opensource.org/licenses/MIT
  */
 
+/**
+ * Modes of transportation. The elements in this array can identify are meant to
+ * differentiate between the various types of road user, the infrastructure
+ * used, the potential payment types and what kind of organisation offers the
+ * service, if any. Modes may have additional fields necessary to complement the
+ * description of the actual vehicle used
+ */
 export const modeValues = [
     'walk',
-    'bicycle',
-    'bicycleElectric',
-    'bicycleBikeSharing',
-    'bicycleBikeSharingElectric',
-    'transitBus', // category C
-    'transitBRT', // category B
-    'transitSchoolBus', // for school lines operated by transit agencies
+    'bicycle', // complementary type: BicycleType
+    'bicyclePassenger',
+    'scooterElectric',
+    'carDriver', // complementary type: CarType
+    'carPassenger',
+    'transitBus', // includes Bus Transit System (BTS) category C
+    'transitBRT', // Bus Rapid Transit category B
+    'transitSchoolBus', // for school lines managed by transit agencies
     'transitStreetCar', // category C
-    'transitFerry',
+    'transitFerry', // Ferry, without a car
     'transitGondola', // category A
     'transitMonorail', // category A
-    'transitRRT', // category A
+    'transitRRT', // Rapid Rail Transit (metro, submway, aerial train), category A
+    'transitLRT', // Light Rail Transit, category B
+    'transitLRRT', // Light Rail Rapid Transit, category A
+    'transitHSR', // High Speed Rail, category A
     'transitRegionalRail', // category A
+    'transitOnDemand', // On demand services (bus, minivan, taxi), usually managed by transit agencies
+    'transitTaxi', // Collective taxi
     'intercityBus',
     'intercityTrain',
     'schoolBus',
-    'otherBus',
-    'carDriver', // type of car (free-floating, station-based, personal car, rental car) separated
-    'carPassenger',
-    'transitTaxi',
+    'otherBus', // Private buses, like hired buses for events, groups, etc.
     'taxi',
-    'uber',
     'paratransit',
-    'plane',
-    'otherActiveMode',
-    'motorcycle',
-    'ferryNoCar',
+    'wheelchair',
+    'mobilityScooter', // Electric wheelchair, mobility scooter, etc.
+    'motorcycle', // includes motorbike, scooter, moped, etc.
     'ferryWithCar',
+    'plane',
+    'otherActiveMode', // Skateboard, kick scooter, rollerblade, etc.
     'other',
     'dontKnow'
 ] as const;
@@ -44,9 +54,8 @@ export type Mode = (typeof modeValues)[number];
 export const mapModeToModeCategory: { [mode in Mode]: ModeCategory } = {
     walk: 'walk',
     bicycle: 'bicycle',
-    bicycleElectric: 'bicycle',
-    bicycleBikeSharing: 'bicycle',
-    bicycleBikeSharingElectric: 'bicycle',
+    bicyclePassenger: 'bicycle',
+    scooterElectric: 'bicycle',
     transitBus: 'transit',
     transitBRT: 'transit',
     transitSchoolBus: 'schoolBus', // TODO: decide  if we should use schoolBus or transit here.
@@ -55,7 +64,11 @@ export const mapModeToModeCategory: { [mode in Mode]: ModeCategory } = {
     transitGondola: 'transit',
     transitMonorail: 'transit',
     transitRRT: 'transit',
+    transitLRT: 'transit',
+    transitLRRT: 'transit',
+    transitHSR: 'transit',
     transitRegionalRail: 'transit',
+    transitOnDemand: 'transit',
     intercityBus: 'transit',
     intercityTrain: 'transit',
     schoolBus: 'schoolBus',
@@ -64,12 +77,12 @@ export const mapModeToModeCategory: { [mode in Mode]: ModeCategory } = {
     carPassenger: 'carPassenger',
     transitTaxi: 'transit',
     taxi: 'taxi',
-    uber: 'taxi',
     paratransit: 'other',
+    wheelchair: 'walk',
+    mobilityScooter: 'walk',
     plane: 'other',
     otherActiveMode: 'other',
     motorcycle: 'other',
-    ferryNoCar: 'transit',
     ferryWithCar: 'other',
     other: 'other',
     dontKnow: 'dontKnow'
@@ -104,6 +117,7 @@ export const driverValues = [
 ] as const;
 export type Driver = (typeof driverValues)[number];
 
+// TODO Add autonomous car type, as well as electric/hybrid/gaz motorization?
 export const carTypeValues = [
     'householdCar',
     'friendFamilyNonHouseholdCar',
@@ -117,3 +131,14 @@ export const carTypeValues = [
     'nonApplicable'
 ] as const;
 export type CarType = (typeof carTypeValues)[number];
+
+export const bicycleTypeValues = [
+    'bicycleNonMotorized',
+    'bicycleElectric',
+    'bicycleBikesharingNonMotorized',
+    'bicycleBikesharingElectric',
+    'other',
+    'dontKnow',
+    'nonApplicable'
+] as const;
+export type BicycleType = (typeof bicycleTypeValues)[number];
