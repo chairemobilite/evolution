0ba30847fe63939f171838cb34ccd369e1c5c5d8
Geneviève Bastien <gbastien@versatic.net>
2024-10-04 12:53:28 -0400

    segments: Add helpers specific to the segment section
    Add types for loopActivities and simple modes (ie individual/private
    modes that are often used for many trips).
    
    Add helpers for survey time to check whether a trip is the return trip
    of a simple single chain, ie origin of first trip is same as the
    destination of second trip and the mode used is a simple mode. Another
    helper returns the unique segment of the previous trip of the chain.
    
    Add unit tests for these functions.

---
+++ packages/evolution-common/src/services/odSurvey/types.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2024, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+
+/**
+ * Activities that do not involve a destination
+ */
+export const loopActivities = ['workOnTheRoad', 'leisureStroll'];
+
+/**
+ * Simple modes, ie individual modes that involve no or private vehicles and are
+ * often used multiple times in a journey
+ */
+export const simpleModes = ['carDriver', 'walk', 'bicycle', 'bicycleElectric', 'scooterElectric'];
---
+++ packages/evolution-frontend/src/services/sections/segments/helpers.ts
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2024, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+
+import _isEqual from 'lodash/isEqual';
+import {
+    Journey,
+    Person,
+    Segment,
+    Trip,
+    UserInterviewAttributes
+} from 'evolution-common/lib/services/interviews/interview';
+import * as helper from 'evolution-common/lib/services/odSurvey/helpers';
+import { loopActivities, simpleModes } from 'evolution-common/lib/services/odSurvey/types';
+import { Optional } from 'evolution-common/lib/types/Optional.type';
+
+/**
+ * Get the mode used in the single segment of the previous trip of the active
+ * one
+ *
+ * @param {Object} options - The options object.
+ * @param {Object} options.interview The interview object
+ * @param {Object} options.person The person these trips belong to
+ * @returns If there is a single mode in the previous trip, return it, otherwise
+ * undefined
+ */
+export const getPreviousTripSingleSegment = ({
+    interview,
+    person
+}: {
+    interview: UserInterviewAttributes;
+    person: Person;
+}): Optional<Segment> => {
+    const journey = helper.getActiveJourney({ interview, person });
+    const trip = helper.getActiveTrip({ interview, journey });
+    if (!journey || !trip) {
+        return undefined;
+    }
+    const previousTrip = helper.getPreviousTrip({ currentTrip: trip, journey });
+    if (previousTrip) {
+        const previousSegments = helper.getSegmentsArray({ trip: previousTrip });
+        if (previousSegments.length === 1) {
+            const previousSegment = previousSegments[0];
+            return previousSegment;
+        }
+    }
+    return undefined;
+};
+
+/**
+ * Return whether these 2 trips are part of a simple chain with a single mode,
+ * ie the origin of the previous trip is the same as the destination of the trip
+ * and there is only one segment in the previous trip that is one of the simple
+ * modes.
+ *
+ * @param {Object} options - The options object.
+ * @param {Trip} options.trip The potential return trip
+ * @param {Trip} options.previousTrip The previous trip that can be part of the
+ * chain
+ * @param {Object} options.journey The journey object that these trips are part
+ * of
+ * @param {Object} options.interview The interview object
+ * @param {Object} options.person The person these trips belong to
+ * @returns Whether the `trip` is the return trip of a simple chain with simple
+ * modes
+ */
+export const isSimpleChainSingleModeReturnTrip = ({
+    trip,
+    previousTrip,
+    journey,
+    interview,
+    person
+}: {
+    trip: Trip;
+    previousTrip: Trip;
+    journey: Journey;
+    interview: UserInterviewAttributes;
+    person: Person;
+}): boolean => {
+    const visitedPlaces = helper.getVisitedPlaces({ journey });
+    const origin = helper.getOrigin({ trip, visitedPlaces });
+    const destination = helper.getDestination({ trip, visitedPlaces });
+    const previousOrigin = helper.getOrigin({ trip: previousTrip, visitedPlaces });
+
+    // If origin or destination is not found, we cannot determine if it is a simple chain
+    if (!origin || !destination || !previousOrigin) {
+        return false;
+    }
+
+    // ignore loop/moving activities:
+    if (
+        (origin.activity && loopActivities.includes(origin.activity)) ||
+        (destination.activity && loopActivities.includes(destination.activity))
+    ) {
+        return false;
+    }
+    // If the trip already has more than one segment, it is not a simple chain
+    const segments = trip.segments || {};
+    const segmentsArray = Object.values(segments);
+    if (segmentsArray.length > 1) {
+        return false;
+    }
+
+    const previousTripOriginGeography = helper.getVisitedPlaceGeography({
+        visitedPlace: previousOrigin,
+        interview,
+        person
+    });
+    const tripDestinationGeography = helper.getVisitedPlaceGeography({ visitedPlace: destination, interview, person });
+    if (
+        previousTripOriginGeography &&
+        tripDestinationGeography &&
+        tripDestinationGeography.geometry &&
+        previousTripOriginGeography.geometry &&
+        _isEqual(previousTripOriginGeography.geometry.coordinates, tripDestinationGeography.geometry.coordinates)
+    ) {
+        const previousTripSegmentsAsArray = helper.getSegmentsArray({ trip: previousTrip });
+        if (
+            previousTripSegmentsAsArray.length === 1 &&
+            previousTripSegmentsAsArray[0].mode &&
+            simpleModes.includes(previousTripSegmentsAsArray[0].mode)
+        ) {
+            // we have a simple chain with single simple mode
+            return true;
+        }
+    }
+    return false;
+};
