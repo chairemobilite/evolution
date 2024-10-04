a3759945b400080204baf697e0bb1b9bfd0d7d75
Geneviève Bastien <gbastien@versatic.net>
2024-10-04 16:56:29 -0400

    segments: Add a widget for the mode question of the segment section
    This adds a generic segment mode question, that generates a radio
    question widget offering a choice of the modes, given the selected
    category.
    
    Labels and mode selection match the mode values of the segments, labels
    are fully localizable, with english and french translation provided.
    
    Some modes have extra conditional function, for example, `wheelchair`
    will only be present for persons that may have disabilities.
    
    We add a map where each mode is mapped to a modePre value. When the user
    selects a `modePre` value, the modes are displayed. If there's only one
    choice available for a given modePre, it is automatically selected.

--- packages/evolution-common/src/services/odSurvey/types.ts
+++ packages/evolution-common/src/services/odSurvey/types.ts
@@ -5,6 +5,8 @@
  * License text available at https://opensource.org/licenses/MIT
  */
 
+import { Mode } from '../baseObjects/attributeTypes/SegmentAttributes';
+
 /**
  * Activities that do not involve a destination
  */
@@ -15,3 +17,62 @@ export const loopActivities = ['workOnTheRoad', 'leisureStroll'];
  * often used multiple times in a journey
  */
 export const simpleModes = ['carDriver', 'walk', 'bicycle', 'bicycleElectric', 'scooterElectric'];
+
+type ModePre = 'carDriver' | 'carPassenger' | 'walk' | 'bicycle' | 'transit' | 'taxi' | 'other' | 'dontKnow';
+
+export const modeToModePreMap: { [mode in Mode]: ModePre[] } = {
+    walk: ['walk'],
+    bicycle: ['bicycle'],
+    bicycleElectric: ['bicycle'],
+    bicycleBikesharing: ['bicycle'],
+    bicycleBikesharingElectric: ['bicycle'],
+    bicyclePassenger: ['bicycle'],
+    scooterElectric: ['bicycle', 'other'],
+    transitBus: ['transit'],
+    transitBRT: ['transit'],
+    transitSchoolBus: ['transit', 'other'],
+    transitStreetCar: ['transit'],
+    transitFerry: ['transit', 'other'],
+    transitGondola: ['transit'],
+    transitMonorail: ['transit'],
+    transitRRT: ['transit'],
+    transitLRT: ['transit'],
+    transitLRRT: ['transit'],
+    transitHSR: ['transit'],
+    transitRegionalRail: ['transit'],
+    intercityBus: ['transit'],
+    intercityTrain: ['transit'],
+    transitOnDemand: ['transit'],
+    schoolBus: ['transit', 'other'],
+    otherBus: ['other'],
+    carDriverPersonal: ['carDriver'],
+    carPassenger: ['carPassenger'],
+    carDriverRental: ['carDriver'],
+    carDriverCarsharingStationBased: ['carDriver'],
+    carDriverCarsharingFreeFloating: ['carDriver'],
+    carDriverCarsharingUnspecified: ['carDriver'],
+    transitTaxi: ['transit', 'taxi'],
+    taxi: ['taxi'],
+    paratransit: ['other', 'transit'],
+    wheelchair: ['walk'],
+    mobilityScooter: ['walk', 'other'],
+    plane: ['other'],
+    otherActiveMode: ['other'],
+    motorcycle: ['other'],
+    ferryWithCar: ['other'],
+    other: ['other'],
+    dontKnow: ['dontKnow'],
+};
+
+export const modePreToModeMap: { [modePre in ModePre]: Mode[] } = Object.entries(modeToModePreMap).reduce(
+    (acc, [mode, modePres]) => {
+        modePres.forEach((modePre) => {
+            if (!acc[modePre]) {
+                acc[modePre] = [];
+            }
+            acc[modePre].push(mode as Mode);
+        });
+        return acc;
+    },
+    {} as { [modePre in ModePre]: Mode[] }
+);
