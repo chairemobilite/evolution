d25366d3ed863f0bb4d121ef468bcf3219e02911
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2024-10-16 13:06:22 -0400

    Move legacy tasks to evolution-backend
    Delete unused/obsolete tasks (jsonFilesParser.task.js is an old transition task unrelated to evolution)
    auditInterviews.task should be run from custom survey (see od_mtl_2023/od_qc_2023 batchAuditInterview tasks)
    TODO: move application config to backend so we can run batchAuditInterview using audits from the custom survey

---
+++ packages/evolution-backend/src/tasks/exportInterviewsJson.task.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import knex from 'chaire-lib-backend/lib/config/shared/db.config';
+import fs from 'fs';
+import taskWrapper from 'chaire-lib-backend/lib/tasks/taskWrapper';
+import { fileManager } from 'chaire-lib-backend/lib/utils/filesystem/fileManager';
+
+const exportJsonFileDirectory = 'exports/interviewsJson';
+
+/**
+ * Export interviews in json files. One json file per interview. No filter (includes invalid/test interviews)
+ */
+const exportInterviews = function (exportJsonFileDirectory) {
+    return knex
+        .select('id', 'uuid', 'responses', 'validated_data')
+        .from('sv_interviews')
+        .orderBy('id')
+        .then((rows) => {
+            for (let i = 0, count = rows.length; i < count; i++) {
+                const interview = rows[i];
+                const responses = interview.responses;
+                responses.id = interview.id;
+                responses.uuid = interview.uuid;
+
+                fileManager.directoryManager.createDirectoryIfNotExists(exportJsonFileDirectory);
+                fs.writeFileSync(
+                    exportJsonFileDirectory + '/' + interview.id + '__' + interview.uuid + '.json',
+                    JSON.stringify(responses, null, 2) // add linefeeds for readability
+                );
+            }
+        });
+};
+
+class ExportInterviewsJSON {
+    async run(argv) {
+        // TODO Support options from the command line
+        await exportInterviews(exportJsonFileDirectory);
+
+        console.log('Interviews JSON exported to:', fileManager.getAbsolutePath(exportJsonFileDirectory));
+    }
+}
+
+taskWrapper(new ExportInterviewsJSON())
+    .then(() => {
+        // eslint-disable-next-line n/no-process-exit
+        process.exit();
+    })
+    .catch((err) => {
+        console.error('Error executing task exportInterviewsJson', err);
+        // eslint-disable-next-line n/no-process-exit
+        process.exit();
+    });
---
+++ packages/evolution-backend/src/tasks/resetInterviewsValidation.task.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import '../config/projectConfig'; // Unused, but must be imported
+import 'chaire-lib-backend/lib/config/dotenv.config';
+import Interviews from '../services/interviews/interviews';
+
+// this task will remove all validated data from every valid interviews and replace validated data with original responses (not modified by validators/admins)
+// You must provide this argument to confirm reset: I WANT TO DELETE ALL VALIDATION WORK
+const confirm = process.argv[2];
+
+if (confirm !== 'I WANT TO DELETE ALL VALIDATION WORK') {
+    console.error(
+        'WARNING!!! This task will remove all validations from the database. To confirm, relaunch the task with this argument: I WANT TO DELETE ALL VALIDATION WORK'
+    );
+    // eslint-disable-next-line n/no-process-exit
+    process.exit();
+}
+
+const run = async function () {
+    await Interviews.resetInterviews(confirm);
+};
+
+run()
+    .then(() => {
+        console.log('Completed task resetInterviewValidation');
+        // eslint-disable-next-line n/no-process-exit
+        process.exit();
+    })
+    .catch((err) => {
+        console.error('Error executing task resetInterviewValidation', err);
+        // eslint-disable-next-line n/no-process-exit
+        process.exit();
+    });
