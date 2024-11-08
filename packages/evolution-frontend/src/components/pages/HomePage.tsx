bf45d3f5ec0c2ff166fae1494dfcc2f05fbc033d
Geneviève Bastien <gbastien@versatic.net>
2024-11-08 13:32:15 -0500

    survey: Add an appContext to the SurveyContext type for generic locales context
    The `appContext` value is passed as prop to the `runClientApp` function
    and set in the `SurveyContext`. The `HomePage` widget uses this as a
    context for the i18n string translations, so it is possible to fine-tune
    these named translation strings to specific "context". This value is
    `undefined` by default and can be anything, either coming from the url,
    or specific to an instance, etc. It is only used to personnalize
    translation strings to this "context".
    
    The `HomePage` widget is turned to a function component instead of a
    class to allow the use the multiple react contexts.

--- packages/evolution-frontend/src/components/pages/HomePage.tsx
+++ packages/evolution-frontend/src/components/pages/HomePage.tsx
@@ -16,6 +16,7 @@ import { startRegister } from '../../actions/Auth';
 import config from 'evolution-common/lib/config/project.config';
 import ConsentAndStartForm from '../pageParts/ConsentAndStartForm';
 import { InterviewContext } from '../../contexts/InterviewContext';
+import { SurveyContext } from '../../contexts/SurveyContext';
 import { CliUser } from 'chaire-lib-common/lib/services/user/userType';
 import { AnyAction } from 'redux';
 
@@ -25,88 +26,73 @@ type HomePageProps = {
     history: History;
     location: Location;
     dispatch: Dispatch<AnyAction>;
-} & WithTranslation;
+};
 
-class HomePage extends React.Component<HomePageProps> {
-    static contextType = InterviewContext;
+const HomePage: React.FunctionComponent<HomePageProps & WithTranslation> = (props: HomePageProps & WithTranslation) => {
+    const { state, dispatch } = React.useContext(InterviewContext);
+    const { appContext } = React.useContext(SurveyContext);
 
-    constructor(props) {
-        super(props);
-
-        if (this.props.isAuthenticated === true) {
-            const goToPage = this.props.user && this.props.user.homePage ? this.props.user.homePage : '/survey';
+    React.useEffect(() => {
+        if (props.isAuthenticated === true) {
+            const goToPage = props.user && props.user.homePage ? props.user.homePage : '/survey';
             // Avoid infinite loops
-            this.props.history.push(goToPage === '' || goToPage === '/' ? '/survey' : goToPage);
+            props.history.push(goToPage === '' || goToPage === '/' ? '/survey' : goToPage);
         }
-    }
-
-    componentDidMount() {
         // If there is a query string, add to the responses
-        if (this.props.location.search && this.props.location.search !== '') {
-            const { state, dispatch } = this.context;
+        if (props.location.search && props.location.search !== '') {
             if (state.status !== 'entering') {
-                dispatch({ type: 'enter', queryData: new URLSearchParams(this.props.location.search) });
+                dispatch({ type: 'enter', queryData: new URLSearchParams(props.location.search) });
             }
         }
-    }
+    }, []);
 
-    afterClick = () => this.props.history.push({ pathname: '/login', search: this.props.location.search });
+    const afterClick = () => props.history.push({ pathname: '/login', search: props.location.search });
 
-    render = () => (
+    return (
         <div className="survey">
             <div style={{ width: '100%', margin: '0 auto', maxWidth: '60em' }}>
                 {config.introBanner && config.bannerPaths && (
                     <div className="main-logo center">
-                        <img
-                            src={config.bannerPaths[this.props.i18n.language]}
-                            style={{ width: '100%' }}
-                            alt="Banner"
-                        />
+                        <img src={config.bannerPaths[props.i18n.language]} style={{ width: '100%' }} alt="Banner" />
                     </div>
                 )}
                 <div style={{ width: '100%', margin: '0 auto', maxWidth: '30em', padding: '0 1rem' }}>
                     <form className="apptr__form" id="survey_form">
                         {!config.introLogoAfterStartButton && config.logoPaths && (
                             <div className="main-logo center">
-                                <img
-                                    src={config.logoPaths[this.props.i18n.language]}
-                                    style={{ width: '100%' }}
-                                    alt="Logo"
-                                />
+                                <img src={config.logoPaths[props.i18n.language]} style={{ width: '100%' }} alt="Logo" />
                             </div>
                         )}
-                        <div dangerouslySetInnerHTML={{ __html: this.props.t('survey:homepage:introduction') }} />
-                        {config.hideStartButtonOnHomePage !== true && (
-                            <ConsentAndStartForm afterClicked={this.afterClick} />
-                        )}
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: props.t('survey:homepage:introduction', { context: appContext })
+                            }}
+                        />
+                        {config.hideStartButtonOnHomePage !== true && <ConsentAndStartForm afterClicked={afterClick} />}
                         {config.introLogoAfterStartButton && config.logoPaths && (
                             <div className="main-logo center">
-                                <img
-                                    src={config.logoPaths[this.props.i18n.language]}
-                                    style={{ width: '100%' }}
-                                    alt="Logo"
-                                />
+                                <img src={config.logoPaths[props.i18n.language]} style={{ width: '100%' }} alt="Logo" />
                             </div>
                         )}
                         {config.introductionTwoParagraph && (
                             <div
                                 dangerouslySetInnerHTML={{
-                                    __html: this.props.t('survey:homepage:introductionParagraphTwo')
+                                    __html: props.t('survey:homepage:introductionParagraphTwo', { context: appContext })
                                 }}
                             />
                         )}
                         <div className="center">
                             {config.languages.map((language) => {
                                 // TODO: make sure it would work with more than two langages (css and formatting)
-                                return this.props.i18n.language !== language ? (
+                                return props.i18n.language !== language ? (
                                     <a
                                         href="#"
                                         className="em"
                                         key={`header__nav-language-${language}`}
                                         onClick={(e) => {
                                             e.preventDefault();
-                                            this.props.i18n.changeLanguage(language);
-                                            moment.locale(this.props.i18n.language);
+                                            props.i18n.changeLanguage(language);
+                                            moment.locale(props.i18n.language);
                                         }}
                                     >
                                         {config.languageNames[language]}
@@ -121,7 +107,7 @@ class HomePage extends React.Component<HomePageProps> {
             </div>
         </div>
     );
-}
+};
 
 const mapStateToProps = (state, _props) => {
     return {
