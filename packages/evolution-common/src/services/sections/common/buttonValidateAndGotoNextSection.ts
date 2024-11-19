bb3eb7fa755f2df78987c5173c00d3bfed99dedd
Geneviève Bastien <gbastien@versatic.net>
2024-11-19 13:41:48 -0500

    survey: add a buttonValidateAndGotoNextSection configuration
    This button is a green checked circle button with a configurable text
    that validates the current section and goes to the next section.
    
    The demo_survey is updated such that all section completion buttons now
    use this widget, with a specific text.

---
+++ packages/evolution-common/src/services/sections/common/buttonValidateAndGotoNextSection.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2024, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+import { ButtonWidgetConfig } from '../../widgets';
+import { ButtonAction } from '../../../utils/helpers';
+import { TFunction } from 'i18next';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+
+/**
+ * Get the configuration for a button that will call the `validateButtonAction`
+ * provided in the options when clicked.
+ *
+ * @param translatableLabel The text to use for this button. It should be a
+ * translatable string key, automatically, its custom version in the
+ * customSurvey namespace will be added to the call
+ * @param options
+ * @returns
+ */
+export const getButtonValidateAndGotoNextSection = (
+    translatableLabel: string,
+    // FIXME: Type this when there is a few more widgets implemented
+    options: {
+        context?: () => string;
+        buttonActions: { validateButtonAction: ButtonAction };
+        iconMapper: { [iconName: string]: IconProp };
+    }
+): ButtonWidgetConfig => {
+    return {
+        type: 'button',
+        // FIXME Is the path important for a button? Should it be configurable, like the text?
+        path: 'buttonValidateGotoNextSection',
+        color: 'green',
+        label: (t: TFunction) => t([`customSurvey:${translatableLabel}`, translatableLabel]),
+        hideWhenRefreshing: true,
+        icon: options.iconMapper['check-circle'],
+        align: 'center',
+        action: options.buttonActions.validateButtonAction
+    };
+};
