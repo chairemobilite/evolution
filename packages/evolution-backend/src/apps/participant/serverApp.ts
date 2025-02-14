9073f4b0392cea80a20e9b158929218a98f303e2
Geneviève Bastien <gbastien@versatic.net>
2025-02-14 10:50:43 -0500

    app: move the participant server app to evolution-backend
    ** BREAKING CHANGE **
    
    Survey's server entry file now need to import the server app function
    from evolution-backend instead of legacy. For example:
    
    ```
    import setupServer from 'evolution-backend/lib/apps/participant';
    ```

--- packages/evolution-backend/src/apps/participant/serverApp.ts
+++ packages/evolution-backend/src/apps/participant/serverApp.ts
@@ -4,33 +4,31 @@
  * This file is licensed under the MIT License.
  * License text available at https://opensource.org/licenses/MIT
  */
-const config           = require('chaire-lib-backend/lib/config/server.config').default;
-const knex             = require('chaire-lib-backend/lib/config/shared/db.config').default;
-const path             = require('path');
+import config from 'chaire-lib-backend/lib/config/server.config';
+import knex from 'chaire-lib-backend/lib/config/shared/db.config';
+import path from 'path';
 import _camelCase from 'lodash/camelCase';
-const express          = require('express');
-const favicon          = require('serve-favicon');
-const expressSession   = require('express-session');
-const KnexSessionStore = require('connect-session-knex')(expressSession);
-const morgan           = require('morgan') // http logger
+import express, { Express } from 'express';
+import favicon from 'serve-favicon';
+import expressSession from 'express-session';
+import connectSessionKnex from 'connect-session-knex';
+import morgan from 'morgan'; // http logger
 import trRoutingRouter from 'chaire-lib-backend/lib/api/trRouting.routes';
-import { participantAuthModel } from 'evolution-backend/lib/services/auth/participantAuthModel';
+import { participantAuthModel } from '../../services/auth/participantAuthModel';
 import configurePassport from 'chaire-lib-backend/lib/config/auth';
-
-//const WebSocket        = require('ws');
-const requestIp        = require('request-ip');
-
+import requestIp from 'request-ip';
 import { directoryManager } from 'chaire-lib-backend/lib/utils/filesystem/directoryManager';
 import authRoutes from 'chaire-lib-backend/lib/api/auth.routes';
-import participantRoutes from 'evolution-backend/lib/apps/participant/routes';
+import participantRoutes from '../../apps/participant/routes';
 
-export const setupServerApp = (app, serverSetupFct = undefined) => {
+const KnexSessionStore = connectSessionKnex(expressSession);
 
+export const setupServerApp = (app: Express, serverSetupFct: (() => void) | undefined = undefined) => {
     // Public directory from which files are served
-    const publicDirectory = path.join(__dirname, '..', '..', '..', '..', '..', '..', 'public');
-    const publicDistDirectory = path.join(publicDirectory, 'dist', config.projectShortname, 'survey', );
+    const publicDirectory = path.join(__dirname, '..', '..', '..', '..', '..', 'public');
+    const publicDistDirectory = path.join(publicDirectory, 'dist', config.projectShortname, 'survey');
     // Local path where locales are stored
-    const localeDirectory = path.join(__dirname, '..', '..', '..', '..', '..', '..', 'locales');
+    const localeDirectory = path.join(__dirname, '..', '..', '..', '..', '..', 'locales');
 
     directoryManager.createDirectoryIfNotExistsAbsolute(publicDistDirectory);
     directoryManager.createDirectoryIfNotExists('logs');
@@ -40,28 +38,36 @@ export const setupServerApp = (app, serverSetupFct = undefined) => {
     directoryManager.createDirectoryIfNotExists('parsers');
     directoryManager.createDirectoryIfNotExists('tasks');
 
-    const indexPath = path.join(publicDistDirectory, `index-survey-${config.projectShortname}${process.env.NODE_ENV === 'test' ? '_test' : ''}.html`);
+    const indexPath = path.join(
+        publicDistDirectory,
+        `index-survey-${config.projectShortname}${process.env.NODE_ENV === 'test' ? '_test' : ''}.html`
+    );
     const publicPath = express.static(publicDistDirectory);
     const localePath = express.static(localeDirectory);
 
     const sessionStore = new KnexSessionStore({
-        knex     : knex,
+        knex: knex,
         tablename: 'sessions' // optional. Defaults to 'sessions'
     });
 
     const session = expressSession({
-        key: "trsession" + _camelCase(config.projectShortname + (process.env.PROJECT_SAMPLE ? "_" + process.env.PROJECT_SAMPLE : "")),
-        secret: process.env.EXPRESS_SESSION_SECRET_KEY,
+        name: 'evsession_' + _camelCase(config.projectShortname),
+        secret: process.env.EXPRESS_SESSION_SECRET_KEY as string,
         resave: false,
         saveUninitialized: false,
         store: sessionStore
     });
     const passport = configurePassport(participantAuthModel);
 
-    app.use(morgan('combined', {
-        // do not log if nolog=true is part of the url params:
-        skip: function (req, res) { return req.url.indexOf('nolog=true') !== -1; }
-    }));
+    app.use(
+        morgan('combined', {
+            // do not log if nolog=true is part of the url params:
+            // FIXME Why would we want to skip logging?
+            skip: function (req, _res) {
+                return req.url.indexOf('nolog=true') !== -1;
+            }
+        })
+    );
     app.use(express.json({ limit: '500mb' }));
     app.use(express.urlencoded({ limit: '500mb', extended: true }));
     app.use(session);
@@ -73,17 +79,17 @@ export const setupServerApp = (app, serverSetupFct = undefined) => {
     // TODO: move all routes to socket routes:
     authRoutes(app, participantAuthModel, passport);
 
-    app.set('trust proxy',true); // allow nginx or other proxy server to send request ip address
+    app.set('trust proxy', true); // allow nginx or other proxy server to send request ip address
 
     // send js and css compressed (gzip) to save bandwidth:
-    app.get('*.js', function(req, res, next) {
+    app.get('*.js', (req, res, next) => {
         req.url = req.url + '.gz';
         res.set('Content-Encoding', 'gzip');
         res.set('Content-Type', 'text/javascript');
         next();
     });
 
-    app.get('*.css', function(req, res, next) {
+    app.get('*.css', (req, res, next) => {
         req.url = req.url + '.gz';
         res.set('Content-Encoding', 'gzip');
         res.set('Content-Type', 'text/css');
@@ -99,30 +105,32 @@ export const setupServerApp = (app, serverSetupFct = undefined) => {
         if (typeof serverSetupFct === 'function') {
             serverSetupFct();
         }
-    } catch(error) {
+    } catch (error) {
         console.log('Error running project specific server setup function: ', error);
     }
     participantRoutes(app);
     // Add the trRouting routes
     trRoutingRouter(app);
 
-    app.get('/incompatible', function (req, res) { res.sendFile(path.join(publicDirectory, 'incompatible.html')); });
-
-    app.get('/ping', function(req, res) {
-    return res.status(200).json({
-        status: 'online'
-    });
+    app.get('/incompatible', (req, res) => {
+        res.sendFile(path.join(publicDirectory, 'incompatible.html'));
     });
 
-    app.get('*', function (req, res) {
-        res.sendFile(indexPath); 
+    app.get('/ping', (req, res) => {
+        return res.status(200).json({
+            status: 'online'
+        });
+    });
+
+    app.get('*', (req, res) => {
+        res.sendFile(indexPath);
     });
 
     return { app, session };
-}
+};
 
-process.on('unhandledRejection', function(err){
-    console.error(err.stack);
+process.on('unhandledRejection', (err) => {
+    console.error(err);
 });
 
 export default setupServerApp;
