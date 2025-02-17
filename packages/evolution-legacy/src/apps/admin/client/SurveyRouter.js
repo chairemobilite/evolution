c6109d551564971937e65344e2c9432deedeb81f
Geneviève Bastien <gbastien@versatic.net>
2025-02-17 16:16:25 -0500

    survey: move the ValidateSurvey component to evolution-frontend
    The component from evolution-legacy is moved to evolution-frontend, with
    its props properly typed some code cleanup to reduce complexity.

--- packages/evolution-legacy/src/apps/admin/client/SurveyRouter.js
+++ packages/evolution-legacy/src/apps/admin/client/SurveyRouter.js
@@ -9,7 +9,7 @@ import { Route, Routes } from 'react-router';
 
 import AdminMonitoringPage from 'evolution-frontend/lib/components/admin/pages/AdminMonitoringPage';
 import AdminValidationPage from '../../../components/admin/Validation';
-import AdminValidateSurveyPage from '../../../components/admin/ValidateSurvey';
+import AdminValidateSurveyPage from 'evolution-frontend/lib/components/admin/pages/ValidateSurvey';
 import UnauthorizedPage from 'chaire-lib-frontend/lib/components/pages/UnauthorizedPage';
 import MaintenancePage from 'chaire-lib-frontend/lib/components/pages/MaintenancePage';
 import { LoginPage as AdminLoginPage } from 'chaire-lib-frontend/lib/components/pages';
