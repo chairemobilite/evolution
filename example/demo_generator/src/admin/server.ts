aa19c795fc4767667a6ea84c82cfc67632e24c55
Samuel Duhaime-Morissette <sduhaimemorissette@gmail.com>
2025-05-07 10:41:28 -0400

    Synchronize the server.ts of the admin and the client
    Rename the file to validationListFilter.ts to make it easier to identify.
    Remove serverFieldUpdate.test.ts because there was no tests yet.

--- example/demo_generator/src/admin/server.ts
+++ example/demo_generator/src/admin/server.ts
@@ -6,22 +6,25 @@
  */
 import path from 'path';
 
-import setupServer from 'evolution-backend/lib/apps/admin';
+import setupServer from 'evolution-backend/lib/apps/participant/index';
 import { setProjectConfig } from 'evolution-backend/lib/config/projectConfig';
 import { registerTranslationDir, addTranslationNamespace } from 'chaire-lib-backend/lib/config/i18next';
+import roleDefinitions from '../survey/server/roleDefinition';
+import validationListFilter from '../survey/server/validationListFilter';
+import serverUpdateCallbacks from '../survey/server/serverFieldUpdate';
+import serverValidations from '../survey/server/serverValidations';
 
 const configureServer = () => {
-    // Default values for each field. Same as default config, but this is an example project, keep it here.
     setProjectConfig({
-        serverUpdateCallbacks: [],
-        serverValidations: undefined,
-        roleDefinitions: undefined
+        serverUpdateCallbacks,
+        serverValidations,
+        roleDefinitions,
+        validationListFilter
     });
-
-}
+};
 
 setupServer(configureServer);
 
 // FIXME Project directory is for runtime, locales should be in the config file (See #420)
-registerTranslationDir(path.join(__dirname, `../../locales/`));
+registerTranslationDir(path.join(__dirname, '../../locales/'));
 addTranslationNamespace('customServer');
--- example/demo_generator/src/server.ts
+++ example/demo_generator/src/server.ts
@@ -10,17 +10,16 @@ import setupServer from 'evolution-backend/lib/apps/participant/index';
 import { setProjectConfig } from 'evolution-backend/lib/config/projectConfig';
 import { registerTranslationDir, addTranslationNamespace } from 'chaire-lib-backend/lib/config/i18next';
 import roleDefinitions from './survey/server/roleDefinition';
-import customFilterForValidation from './survey/server/serverConfiguration';
+import validationListFilter from './survey/server/validationListFilter';
 import serverUpdateCallbacks from './survey/server/serverFieldUpdate';
 import serverValidations from './survey/server/serverValidations';
 
 const configureServer = () => {
-    // Default values for each field. Same as default config, but this is an example project, keep it here.
     setProjectConfig({
         serverUpdateCallbacks,
         serverValidations,
         roleDefinitions,
-        validationListFilter: customFilterForValidation
+        validationListFilter
     });
 };
 
