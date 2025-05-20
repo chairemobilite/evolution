d0fb0a2cf414afcc00567883e3c69736703f1949
Geneviève Bastien <gbastien@versatic.net>
2025-05-20 14:33:16 -0400

    ci: add sequential tests to the CI
    Using a postgis database, this executes Evolution's sequential test
    for every pull request.
    
    Update the participants test so that the IDs of the pre-inserted
    participants can use the sequential DB numbering and be saved for later
    in the tests. Otherwise, the IDs may clash with the auto-increment ID
    from the DB at some point.

--- packages/evolution-backend/src/models/__tests__/participants.db.test.ts
+++ packages/evolution-backend/src/models/__tests__/participants.db.test.ts
@@ -13,7 +13,6 @@ import { truncate } from 'chaire-lib-backend/lib/models/db/default.db.queries';
 import { ParticipantAttributes } from '../../services/participants/participant';
 
 const participant1 = {
-    id: 1,
     email: 'test@test.org',
     username: 'p1',
     preferences: { lang: 'fr' },
@@ -21,7 +20,6 @@ const participant1 = {
 };
 
 const participant2 = {
-    id: 2,
     email: 'test@example.org',
     username: 'p2',
     preferences: { lang: 'fr' },
@@ -34,11 +32,21 @@ const participantToInsert = {
     first_name: 'Foo'
 };
 
+// Keep participant IDs in an object as otherwise the parameterized test use
+// values from before the participants are inserted in the `beforeAll` hook
+const participantIds = {
+    participantId1: -1,
+    participantId2: -1
+}
+
 beforeAll(async () => {
     jest.setTimeout(10000);
     await truncate(knex, 'sv_participants');
-    await knex('sv_participants').insert(participant1);
-    await knex('sv_participants').insert(participant2);
+    // Insert 2 participants and keep their IDs
+    const part1Ret = await knex('sv_participants').insert(participant1).returning('id');
+    participantIds.participantId1 = part1Ret[0].id;
+    const part2Ret = await knex('sv_participants').insert(participant2).returning('id');
+    participantIds.participantId2 = part2Ret[0].id;
 });
 
 afterAll(async() => {
@@ -66,11 +74,12 @@ each([
 });
 
 each([
-    [participant1.id, participant1 ],
-    [participant2.id, participant2],
+    ['participantId1', participant1],
+    ['participantId2', participant2],
     [50, undefined]
-]).test('getById', async(id, expected) => {
-    const user = await dbQueries.getById(id);
+]).test('getById %s', async(id, expected) => {
+    const participantId = typeof id === 'string' ? participantIds[id] : id;
+    const user = await dbQueries.getById(participantId);
     if (expected === undefined) {
         expect(user).toBeUndefined();
     } else {
@@ -98,15 +107,15 @@ test('Update participant', async () => {
 
     // Update the first name
     const newName = 'Newname';
-    const { id, first_name, ...origUser } = await dbQueries.getById(participant1.id) as ParticipantAttributes;
+    const { id, first_name, ...origUser } = await dbQueries.getById(participantIds.participantId1) as ParticipantAttributes;
     const updatedAttributes = _cloneDeep(origUser) as ParticipantAttributes;
     updatedAttributes.first_name = newName;
-    expect(await dbQueries.update(participant1.id, updatedAttributes)).toEqual(true);
-    const updatedUser = await dbQueries.getById(participant1.id) as ParticipantAttributes;
+    expect(await dbQueries.update(participantIds.participantId1, updatedAttributes)).toEqual(true);
+    const updatedUser = await dbQueries.getById(participantIds.participantId1) as ParticipantAttributes;
     expect(updatedUser.first_name).toEqual(newName);
 
     // Try to update id or email
-    expect(await dbQueries.update(participant1.id, { email: 'new@test.org', id: participant1.id + 10 })).toEqual(false);
-    const updatedUser2 = await dbQueries.getById(participant1.id) as ParticipantAttributes;
+    expect(await dbQueries.update(participantIds.participantId1, { email: 'new@test.org', id: participantIds.participantId1 + 10 })).toEqual(false);
+    const updatedUser2 = await dbQueries.getById(participantIds.participantId1) as ParticipantAttributes;
     expect(updatedUser2).toEqual(updatedUser);
 });
