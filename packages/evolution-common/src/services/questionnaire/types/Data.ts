4b900fb02bb6c0120938a249650bc1c1932d18ab
Geneviève Bastien <gbastien@versatic.net>
2025-05-23 14:56:23 -0400

    response: add the iterationContext property to section navigation
    When a section is part of a repeated block, it may be visited many times
    with different contexts, depending on the currently selected active
    survey object. Instead of saving the complete path of the section, we
    separate the section name from the iteration context in the data to
    facilitate navigation.

--- packages/evolution-common/src/services/questionnaire/types/Data.ts
+++ packages/evolution-common/src/services/questionnaire/types/Data.ts
@@ -175,11 +175,12 @@ export type InterviewResponse = {
 
     _sections?: {
         [sectionName: string]: SectionStatus & {
-            [subSection: string]: SectionStatus;
+            [iterationContextString: string]: SectionStatus;
         };
     } & {
         _actions: {
             section: string;
+            iterationContext?: string[];
             action: 'start';
             ts: number;
         }[];
