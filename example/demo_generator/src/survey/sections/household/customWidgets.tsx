e119549b58f013449db6ca7e1773b632512cea8a
Samuel Duhaime-Morissette <sduhaimemorissette@gmail.com>
2025-05-27 15:42:06 -0400

    Add Household section in Demo_Generator
    Test the household section with 1 and 2 persons.

---
+++ example/demo_generator/src/survey/sections/household/customWidgets.tsx
@@ -0,0 +1,49 @@
+import * as surveyHelperNew from 'evolution-common/lib/utils/helpers';
+import { GroupConfig } from 'evolution-common/lib/services/questionnaire/types';
+import { householdMembersWidgetsNames } from './widgetsNames';
+
+// TODO: Implement the logic to count the number of persons in the household (or with preload file)
+export const householdMembers: GroupConfig = {
+    type: 'group',
+    path: 'household.persons',
+    title: {
+        fr: 'Membres du ménage',
+        en: 'Household members'
+    },
+    name: {
+        fr: function (groupedObject: any, sequence, interview) {
+            const householdSize = surveyHelperNew.getResponse(interview, 'household.size', 1);
+            if (householdSize === 1) {
+                return 'Veuillez entrer les informations suivantes:';
+            }
+            return `Personne ${sequence || groupedObject['_sequence']} ${
+                groupedObject.nickname ? `• **${groupedObject.nickname}**` : ''
+            }`;
+        },
+        en: function (groupedObject: any, sequence, interview) {
+            const householdSize = surveyHelperNew.getResponse(interview, 'household.size', 1);
+            if (householdSize === 1) {
+                return 'Please enter the following informations:';
+            }
+            return `Person ${sequence || groupedObject['_sequence']} ${
+                groupedObject.nickname ? `• **${groupedObject.nickname}**` : ''
+            }`;
+        }
+    },
+    showGroupedObjectDeleteButton: function (interview, path) {
+        return true;
+        // const countPersons = helper.countPersons(interview);
+        // const householdSize = surveyHelperNew.getResponse(interview, 'household.size', null);
+        // const householdSizeNum = householdSize ? Number(householdSize) : undefined;
+        // return householdSizeNum ? countPersons > householdSizeNum : false;
+    },
+    showGroupedObjectAddButton: function (interview, path) {
+        return true;
+    },
+    groupedObjectAddButtonLabel: {
+        fr: 'Ajouter une personne manquante',
+        en: 'Add a missing person'
+    },
+    addButtonSize: 'small',
+    widgets: householdMembersWidgetsNames
+};
