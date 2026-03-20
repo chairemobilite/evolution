6eca899f23d97e19965e3ddef6d8d7a44cddd143
Geneviève Bastien <gbastien@versatic.net>
2026-03-20 08:58:01 -0400

    eslint: add rules specific for tests
    ** To be documented when we agree **

--- packages/evolution-common/.prettierrc.js
+++ packages/evolution-common/.prettierrc.js
@@ -5,5 +5,16 @@
  * License text available at https://opensource.org/licenses/MIT
  */
 module.exports = {
-  ...require('../../configs/base.prettierrc')
+  ...require('../../configs/base.prettierrc'),
+  // FIXME Move to the base config when we agree on it.
+  overrides: [
+    {
+      files: ['**/__tests__/*.{ts,tsx}', '**/*.{test,spec}.{ts,tsx}'],
+      options: {
+        // For test files, we allow longer lines and do not break objects (e.g. expected values) into multiple lines
+        printWidth: 150,
+        objectWrap: 'collapse'
+      }
+    }
+  ]
 }
