cc2478d4a207e42ec6963a263d4a8eea04a1701c
Geneviève Bastien <gbastien@versatic.net>
2026-07-31 14:39:29 -0400

    demo_survey: add the interviewer search pages to admin page
    This adds an "Interviewers" link to the admin links, so the search by
    access code and interviewer creation link are available to all admin
    users with required permissions.

--- example/demo_survey/src/admin/app-admin.tsx
+++ example/demo_survey/src/admin/app-admin.tsx
@@ -5,12 +5,19 @@ import appConfig, { EvolutionApplicationConfiguration } from 'evolution-frontend
 import { surveySections, widgetsConfig } from '../survey/questionnaire';
 import projectHelpers from '../survey/helper';
 
+// TODO This is a workaround to get the links to the user, until some more complete solution is implemented (see https://github.com/chairemobilite/transition/issues/1516)
+const pages = [
+    ...appConfig.pages,
+    { path: '/interviews', permissions: { Interviews: ['read', 'update'] }, title: 'survey:Interviewers' }
+];
+
 setApplicationConfiguration<EvolutionApplicationConfiguration>({
     sections: surveySections,
     widgets: widgetsConfig as any,
     allowedUrlFields: ['source', 'household.carNumber'],
     projectHelpers: projectHelpers as any,
-    templateMapping: appConfig.templateMapping
+    templateMapping: appConfig.templateMapping,
+    pages
 });
 
 runClientApp();
