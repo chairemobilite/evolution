c1171d9e89640307da0b027080ab1b98f899b5a5
Geneviève Bastien <gbastien@versatic.net>
2026-09-04 17:08:46 -0400

    segments: initialize `segments` object instead of just single segment
    fixes #1943
    
    When a trip is initialized or re-initialized, instead of adding a single
    segment object, we set the whole trip's `segments` object. In the case
    of trips activated for the first time, it does not change anything, but
    for trips whose origin/destination changes, it does not rely on key
    order anymore to unset the segments before creating new ones. It will
    directly overwrite previous segments.

--- packages/evolution-frontend/src/components/survey/sectionTemplates/__tests__/TripsAndSegmentsSection.test.tsx
+++ packages/evolution-frontend/src/components/survey/sectionTemplates/__tests__/TripsAndSegmentsSection.test.tsx
@@ -433,12 +433,10 @@ describe('SegmentsSection behavior', () => {
         fireEvent.click(editButton!);
         const valuesByPath = (props.startUpdateInterview as jest.Mock).mock.calls[0][0].valuesByPath;
         expect(valuesByPath['response._activeTripId']).toEqual('trip2');
-        const newSegmentPath = Object.keys(valuesByPath).find((path: string) =>
-            path.startsWith('response.household.persons.person1.journeys.journey1.trips.trip2.segments.')
-        );
-        expect(newSegmentPath).toBeDefined();
-        const segmentId = newSegmentPath!.split('.').pop();
-        expect(valuesByPath[newSegmentPath!]).toEqual({ _uuid: segmentId, _sequence: 1, _isNew: true });
+        const segments = valuesByPath['response.household.persons.person1.journeys.journey1.trips.trip2.segments'];
+        expect(segments).toBeDefined();
+        const segmentId = Object.keys(segments)[0];
+        expect(segments).toEqual({ [segmentId]: { _uuid: segmentId, _sequence: 1, _isNew: true } });
         expect(valuesByPath[`validations.household.persons.person1.journeys.journey1.trips.trip2.segments.${segmentId}`]).toEqual({});
     });
 
