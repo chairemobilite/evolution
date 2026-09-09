81de0240b0708abe22b214019ddb513dff580991
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2026-09-09 13:20:29 -0400

    [backport] Admin: do not disable household/interview if error
    Show the audit check errors that is stopping the object creation instead
    
    closes #1961

--- packages/evolution-frontend/src/components/admin/validations/InterviewStats.tsx
+++ packages/evolution-frontend/src/components/admin/validations/InterviewStats.tsx
@@ -81,24 +81,6 @@ const InterviewStats = (props: InterviewStatsProps) => {
         );
     }
 
-    if (!household) {
-        return (
-            <div className="admin__interview-stats">
-                <h4>{t('interviewStats.errors.error')}</h4>
-                <p className="_red">{t('interviewStats.errors.householdNotAvailable')}</p>
-            </div>
-        );
-    }
-
-    if (!home) {
-        return (
-            <div className="admin__interview-stats">
-                <h4>{t('interviewStats.errors.error')}</h4>
-                <p className="_red">{t('interviewStats.errors.homeNotAvailable')}</p>
-            </div>
-        );
-    }
-
     const persons: { [key: string]: Person } = household?.members
         ? household.members.reduce(
             (acc, person) => {
@@ -127,7 +109,7 @@ const InterviewStats = (props: InterviewStatsProps) => {
     });
     const personInheritedStatus = getInheritedStatusForDisplay(reviewDecisionStatusByObject, {
         objectType: 'household',
-        objectUuid: household._uuid,
+        objectUuid: household?._uuid,
         inheritedStatus: interviewStatusForDisplay
     });
 
