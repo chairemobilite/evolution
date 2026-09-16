28b5d5a0181cae42f5332d9aab0432bb6f8505ca
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2026-09-16 18:00:21 -0400

    Survey object names should all be in the filter
    Journey was missing from the audit check filter.
    All other survey object names are now present.
    
    closes #2017

--- packages/evolution-frontend/src/components/admin/validations/ValidationAuditFilter.tsx
+++ packages/evolution-frontend/src/components/admin/validations/ValidationAuditFilter.tsx
@@ -12,6 +12,7 @@ import Select from 'react-select';
 
 import { AuditStatsByLevelAndObjectType } from 'evolution-common/lib/services/audits/types';
 import { InterviewListAttributes } from 'evolution-common/lib/services/questionnaire/types';
+import { surveyObjectNames } from 'evolution-common/lib/services/baseObjects/types';
 
 const auditLevels = ['error', 'warning', 'info'];
 
@@ -24,17 +25,6 @@ type GroupType = {
     label: string; // group label
     options: OptionType[];
 };
-
-const surveyObjectsSortOrder = [
-    'interview',
-    'home',
-    'household',
-    'person',
-    'vehicle',
-    'visitedPlace',
-    'trip',
-    'segment'
-];
 
 /**
  * Textbox input for column filter
@@ -114,8 +104,7 @@ export const ValidationAuditFilter = ({
         selectedAuditKeyByLevel[level] = [];
         const groupedChoices: GroupType[] = [];
 
-        for (let i = 0, countI = surveyObjectsSortOrder.length; i < countI; i++) {
-            const surveyObject = surveyObjectsSortOrder[i];
+        for (const surveyObject of surveyObjectNames) {
             const choices: OptionType[] = [];
             const audits = auditsBySurveyObject[surveyObject] || [];
             for (let j = 0, countJ = audits.length; j < countJ; j++) {
